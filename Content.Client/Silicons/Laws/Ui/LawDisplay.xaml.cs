4311b8f12a0682ae46f7eb1c740579c1caabec9c
Tayrtahn <tayrtahn@gmail.com>
2024-06-09 02:57:07 +0300

    Strip markdown from silicon laws before saying them (#28596)
    (cherry picked from commit 5f91c3f27f2d6d8cb67413d2e85f3fa13a38d89a)

--- Content.Client/Silicons/Laws/Ui/LawDisplay.xaml.cs
+++ Content.Client/Silicons/Laws/Ui/LawDisplay.xaml.cs
@@ -10,6 +10,7 @@ using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using Robust.Client.UserInterface.XAML;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
 
 namespace Content.Client.Silicons.Laws.Ui;
 
@@ -28,6 +29,8 @@ public sealed partial class LawDisplay : Control
         var identifier = law.LawIdentifierOverride ?? $"{law.Order}";
         var lawIdentifier = Loc.GetString("laws-ui-law-header", ("id", identifier));
         var lawDescription = Loc.GetString(law.LawString);
+        var lawIdentifierPlaintext = FormattedMessage.RemoveMarkupPermissive(lawIdentifier);
+        var lawDescriptionPlaintext = FormattedMessage.RemoveMarkupPermissive(lawDescription);
 
         LawNumberLabel.SetMarkup(lawIdentifier);
         LawLabel.SetMessage(lawDescription);
@@ -47,7 +50,7 @@ public sealed partial class LawDisplay : Control
 
         localButton.OnPressed += _ =>
         {
-            _chatManager.SendMessage($"{lawIdentifier}: {lawDescription}", ChatSelectChannel.Local);
+            _chatManager.SendMessage($"{lawIdentifierPlaintext}: {lawDescriptionPlaintext}", ChatSelectChannel.Local);
         };
 
         LawAnnouncementButtons.AddChild(localButton);
@@ -74,9 +77,9 @@ public sealed partial class LawDisplay : Control
                 switch (radioChannel)
                 {
                     case SharedChatSystem.CommonChannel:
-                        _chatManager.SendMessage($"{SharedChatSystem.RadioCommonPrefix} {lawIdentifier}: {lawDescription}", ChatSelectChannel.Radio); break;
+                        _chatManager.SendMessage($"{SharedChatSystem.RadioCommonPrefix} {lawIdentifierPlaintext}: {lawDescriptionPlaintext}", ChatSelectChannel.Radio); break;
                     default:
-                        _chatManager.SendMessage($"{SharedChatSystem.RadioChannelPrefix}{radioChannelProto.KeyCodes.First()} {lawIdentifier}: {lawDescription}", ChatSelectChannel.Radio); break;
+                        _chatManager.SendMessage($"{SharedChatSystem.RadioChannelPrefix}{radioChannelProto.KeyCodes.First()} {lawIdentifierPlaintext}: {lawDescriptionPlaintext}", ChatSelectChannel.Radio); break;
                 }
             };
 
