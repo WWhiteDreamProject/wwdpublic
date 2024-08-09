bb93346261b7dd6663977826ce0d7124178f3f94
TsjipTsjip <19798667+TsjipTsjip@users.noreply.github.com>
2024-08-09 08:24:05 +0200

    Log enrichment: Explosion damage to players (#29762)
    * Log enrichment: Explosion damage to players
    
    * Update Content.Server/Explosion/EntitySystems/ExplosionSystem.Processing.cs
    
    Co-authored-by: Chief-Engineer <119664036+Chief-Engineer@users.noreply.github.com>
    
    * Revert to total damage variant only, currently serialised as string.
    
    * Make this its own log type.
    
    ---------
    
    Co-authored-by: Chief-Engineer <119664036+Chief-Engineer@users.noreply.github.com>
    (cherry picked from commit 736325a31f90cb1ffde51f5114a166bf87b71c8c)

--- Content.Shared.Database/LogType.cs
+++ Content.Shared.Database/LogType.cs
@@ -112,6 +112,12 @@ public enum LogType
     /// Storage & entity-storage related interactions
     /// </summary>
     Storage = 93,
+
+    /// <summary>
+    /// A player got hit by an explosion and was dealt damage.
+    /// </summary>
+    ExplosionHit = 94,
+
     // WD EDIT
     AspectAnnounced = 200,
     AspectStarted = 201,
