f65bcf7efbe2be543cb2e26bc902163b4cef9617
Spatison <137375981+Spatison@users.noreply.github.com>
2024-08-22 12:44:06 +0300

    fix: rotation

--- Content.Client/_White/Standing/LayingDownSystem.cs
+++ Content.Client/_White/Standing/LayingDownSystem.cs
@@ -39,17 +39,23 @@ public sealed class LayingDownSystem : SharedLayingDownSystem
         if (_animation.HasRunningAnimation(uid, "rotate"))
             return;
 
-        if (!TryComp<TransformComponent>(uid, out var transform) || !TryComp<SpriteComponent>(uid, out var sprite))
+        if (!TryComp<TransformComponent>(uid, out var transform)
+            || !TryComp<SpriteComponent>(uid, out var sprite)
+            || !TryComp<RotationVisualsComponent>(uid, out var rotationVisuals))
+        {
             return;
+        }
 
         var rotation = transform.LocalRotation + (_eyeManager.CurrentEye.Rotation - (transform.LocalRotation - transform.WorldRotation));
 
         if (rotation.GetDir() is Direction.SouthEast or Direction.East or Direction.NorthEast or Direction.North)
         {
+            rotationVisuals.HorizontalRotation = Angle.FromDegrees(270);
             sprite.Rotation = Angle.FromDegrees(270);
             return;
         }
 
+        rotationVisuals.HorizontalRotation = Angle.FromDegrees(90);
         sprite.Rotation = Angle.FromDegrees(90);
     }
 
