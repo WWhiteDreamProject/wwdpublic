cd9a247409d63950e556342774b24f9c39a15a0b
vanx <#vanxxxx>
2024-08-23 18:09:01 +0300

    VisionLimit component, system & overlay

---
+++ Content.Client/_White/VisionLimit/Overlays/VisionLimitOverlay.cs
@@ -0,0 +1,53 @@
+﻿using Robust.Client.Graphics;
+using Robust.Client.Player;
+using Robust.Shared.Enums;
+using Robust.Shared.Prototypes;
+
+namespace Content.Client._White.VisionLimit.Overlays
+
+{
+    public sealed class VisionLimitOverlay : Overlay
+    {
+        [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+        [Dependency] private readonly IPlayerManager _playerManager = default!;
+        [Dependency] private readonly IEntityManager _entityManager = default!;
+
+        public float VisionLimitRadius;
+
+        public override bool RequestScreenTexture => true;
+        public override OverlaySpace Space => OverlaySpace.WorldSpace;
+        private readonly ShaderInstance _circleMaskShader;
+
+        public VisionLimitOverlay()
+        {
+            IoCManager.InjectDependencies(this);
+            _circleMaskShader = _prototypeManager.Index<ShaderPrototype>("CircleMask").InstanceUnique();
+        }
+
+        protected override void Draw(in OverlayDrawArgs args)
+        {
+            if (ScreenTexture == null || VisionLimitRadius == 0)
+                return;
+
+            var playerEntity = _playerManager.LocalSession?.AttachedEntity;
+
+            if (playerEntity == null)
+                return;
+
+            if (_entityManager.TryGetComponent<EyeComponent>(playerEntity, out var content))
+            {
+                _circleMaskShader.SetParameter("Zoom", content.Zoom.X);
+
+                _circleMaskShader.SetParameter("CircleRadius", 6.5f); // It's relative, close enough
+                _circleMaskShader.SetParameter("CircleMinDist", VisionLimitRadius * 10); // to 1 unit = 1 tile
+            }
+
+            var worldHandle = args.WorldHandle;
+            var viewport = args.WorldBounds;
+
+            worldHandle.UseShader(_circleMaskShader);
+            worldHandle.DrawRect(viewport, Color.White);
+            worldHandle.UseShader(null);
+        }
+    }
+}
---
+++ Content.Client/_White/VisionLimit/Systems/VisionLimitSystem.cs
@@ -0,0 +1,53 @@
+﻿using Content.Client._White.VisionLimit.Overlays;
+using Content.Shared.Clothing;
+using Content.Shared.Inventory.Events;
+using Robust.Client.Graphics;
+using VisionLimitComponent = Content.Shared._White.VisionLimit.Components.VisionLimitComponent;
+
+namespace Content.Client._White.VisionLimit.Systems;
+
+public sealed class VisionLimitSystem : EntitySystem
+{
+    [Dependency] private readonly IOverlayManager _overlayMan = default!;
+
+    private VisionLimitOverlay _overlay = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<VisionLimitComponent, GotEquippedEvent>(OnGotEquipped);
+        SubscribeLocalEvent<VisionLimitComponent, GotUnequippedEvent>(OnGotUnequipped);
+
+        SubscribeLocalEvent<VisionLimitComponent, ItemMaskToggledEvent>(OnMaskToggled);
+
+        _overlay = new();
+    }
+
+    private void OnGotEquipped(EntityUid uid, VisionLimitComponent comp, GotEquippedEvent args)
+    {
+        UpdateOverlay(comp, true);
+    }
+
+    private void OnGotUnequipped(EntityUid uid, VisionLimitComponent comp, GotUnequippedEvent args)
+    {
+        UpdateOverlay(comp, false);
+    }
+
+    private void OnMaskToggled(EntityUid uid, VisionLimitComponent comp, ItemMaskToggledEvent args)
+    {
+        UpdateOverlay(comp, !args.IsToggled);
+    }
+
+    private void UpdateOverlay(VisionLimitComponent comp, bool active)
+    {
+        if (active)
+        {
+            _overlay.VisionLimitRadius = comp.Radius; // Assign the radius value from the component to the overlay
+            _overlayMan.AddOverlay(_overlay);
+        }
+        else
+            _overlayMan.RemoveOverlay(_overlay);
+    }
+
+}
