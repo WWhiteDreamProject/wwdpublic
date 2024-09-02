6babeb4e2ca92ff6e02e2d761762393848d9d3b3
Spatison <137375981+Spatison@users.noreply.github.com>
2024-09-02 19:59:42 +0300

    tweak: neglecting time when using scrap

--- Content.Shared/Prying/Systems/PryingSystem.cs
+++ Content.Shared/Prying/Systems/PryingSystem.cs
@@ -133,7 +133,14 @@ public sealed class PryingSystem : EntitySystem
         var modEv = new GetPryTimeModifierEvent(user);
 
         RaiseLocalEvent(target, ref modEv);
-        var doAfterArgs = new DoAfterArgs(EntityManager, user, TimeSpan.FromSeconds(modEv.BaseTime * modEv.PryTimeModifier / toolModifier), new DoorPryDoAfterEvent(), target, target, tool)
+
+        // WD EDIT START
+        var time = modEv.BaseTime * modEv.PryTimeModifier / toolModifier;
+
+        if (time <= modEv.Neglect)
+            time = 0;
+
+        var doAfterArgs = new DoAfterArgs(EntityManager, user, TimeSpan.FromSeconds(time), new DoorPryDoAfterEvent(), target, target, tool) // WD EDIT END
         {
             BreakOnDamage = true,
             BreakOnUserMove = true,
@@ -167,7 +174,7 @@ public sealed class PryingSystem : EntitySystem
             return;
         }
 
-        if (args.Used != null && comp != null)
+        if (args.Used != null && comp != null && door.State is not DoorState.Closing and not DoorState.Opening) // WD EDIT
         {
             _audioSystem.PlayPredicted(comp.UseSound, args.Used.Value, args.User);
         }
