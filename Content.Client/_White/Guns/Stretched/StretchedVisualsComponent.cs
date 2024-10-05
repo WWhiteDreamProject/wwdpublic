6c8993e4d10a992ae70c5094ff32624bccf708e0
Spatison <137375981+Spatison@users.noreply.github.com>
2024-10-05 22:52:08 +0300

    fix

--- Content.Client/_White/Guns/Stretched/StretchedVisualsComponent.cs
+++ Content.Client/_White/Guns/Stretched/StretchedVisualsComponent.cs
@@ -3,12 +3,12 @@ namespace Content.Client._White.Guns.Stretched;
 [RegisterComponent, Access(typeof(StretchedVisualizerSystem))]
 public sealed partial class StretchedVisualsComponent : Component
 {
-    [DataField]
+    [DataField(required: true)]
     public string? LoadedState;
 
-    [DataField]
+    [DataField(required: true)]
     public string? StretchedState;
 
-    [DataField]
+    [DataField(required: true)]
     public string? UnstrungState;
 }
