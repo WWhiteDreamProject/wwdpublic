615891c4a18c73d82f364eee5301585e5a3a7969
Remuchi <72476615+Remuchi@users.noreply.github.com>
2024-10-27 23:45:05 +0700

    [Feature] Telescopic Baton (#1144)
    <!--
    This is a semi-strict format, you can add/remove sections as needed but
    the order/format should be kept the same
    Remove these comments before submitting
    -->
    
    # Description
    
    Introduces a telescopic baton - a new self-defense weapon for command
    staff
    
    DEVELOPERS:
    * Adds a way to apply status effects with pre-made component.
    
    AWAITING DISCUSSION:
    * Currently, the system works that players are rewarded for their skill
    and ability to click precisely. The batton will knockdown someone only
    in first 1.5 seconds after extending it. If you fail to do so, you'll
    have to retract it and extend once again, which takes about 4 seconds
    total.
    
    ---
    <details><summary><h1>Media</h1></summary>
    <p>
    
    https://github.com/user-attachments/assets/cc19bf44-391e-4d27-9be9-96d3d77bebc8
    
    </p>
    </details>
    
    ---
    
    # Changelog
    
    :cl:
    - add: Added telescopic baton - a self-defense weapon for Command staff.
    
    ---------
    
    Signed-off-by: Remuchi <RemuchiOfficial@gmail.com>
    Signed-off-by: VMSolidus <evilexecutive@gmail.com>
    Co-authored-by: VMSolidus <evilexecutive@gmail.com>
    (cherry picked from commit 15e46cf7555313dc79b86241e9881c000918782a)

--- Content.Shared/Standing/SharedLayingDownSystem.cs
+++ Content.Shared/Standing/SharedLayingDownSystem.cs
@@ -203,6 +203,7 @@ public abstract class SharedLayingDownSystem : EntitySystem
 [Serializable, NetSerializable]
 public sealed partial class StandingUpDoAfterEvent : SimpleDoAfterEvent;
 
+[Serializable, NetSerializable]
 public enum DropHeldItemsBehavior : byte
 {
     NoDrop,
