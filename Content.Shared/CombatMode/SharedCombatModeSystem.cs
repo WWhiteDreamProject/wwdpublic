b56129d6b92025430f799b1150754201f08b4bb8
RedFoxIV <38788538+RedFoxIV@users.noreply.github.com>
2024-12-06 20:29:31 +0300

    кричу

--- Content.Shared/CombatMode/SharedCombatModeSystem.cs
+++ Content.Shared/CombatMode/SharedCombatModeSystem.cs
@@ -93,7 +93,7 @@ public abstract class SharedCombatModeSystem : EntitySystem
         if (value)
         {
             var rot = EnsureComp<MouseRotatorComponent>(uid);
-            if (TryComp<CombatModeComponent>(uid, out var comp) && comp.smoothRotation) // no idea under which (intended) circumstances this can fail (if any), so i'll avoid Comp<>().
+            if (TryComp<CombatModeComponent>(uid, out var comp) && comp.SmoothRotation) // no idea under which (intended) circumstances this can fail (if any), so i'll avoid Comp<>().
             {
                 rot.AngleTolerance = Angle.FromDegrees(1); // arbitrary
                 rot.Simple4DirMode = false;
