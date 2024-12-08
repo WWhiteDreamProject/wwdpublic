1a35baa81569cf9de1e942477fc7cbed7fdade07
AwareFoxy <vgstecker@gmail.com>
2024-12-08 08:17:48 +0100

    WD Edit

--- Content.Packaging/ServerPackaging.cs
+++ Content.Packaging/ServerPackaging.cs
@@ -44,7 +44,7 @@ public static class ServerPackaging
         // Python script had Npgsql. though we want Npgsql.dll as well soooo
         "Npgsql",
         "Microsoft",
-        "Discord.Net",
+        "Discord.Net", // WD-Edit
     };
 
     private static readonly List<string> ServerNotExtraAssemblies = new()
