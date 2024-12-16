33cb55cd1945856cb29e8c42c9410c26c8670b67
TGRCDev <tgrc@tgrc.dev>
2024-12-16 06:57:11 -0500

    Bug fixes for Store UIs with multiple currencies (#33565)
    Fixed stores with multiple currencies having bad formatting and non-functional withdraw screens

--- Content.Client/Store/Ui/StoreMenu.xaml.cs
+++ Content.Client/Store/Ui/StoreMenu.xaml.cs
@@ -57,7 +57,7 @@ public sealed partial class StoreMenu : DefaultWindow
         foreach (var ((_, amount), proto) in currency)
         {
             balanceStr += Loc.GetString("store-ui-balance-display", ("amount", amount),
-                ("currency", Loc.GetString(proto.DisplayName, ("amount", 1))));
+                ("currency", Loc.GetString(proto.DisplayName, ("amount", 1)))) + "\n";
         }
 
         BalanceInfo.SetMarkup(balanceStr.TrimEnd());
@@ -66,7 +66,10 @@ public sealed partial class StoreMenu : DefaultWindow
         foreach (var type in currency)
         {
             if (type.Value.CanWithdraw && type.Value.Cash != null && type.Key.Item2 > 0)
+            {
                 disabled = false;
+                break;
+            }
         }
 
         WithdrawButton.Disabled = disabled;
