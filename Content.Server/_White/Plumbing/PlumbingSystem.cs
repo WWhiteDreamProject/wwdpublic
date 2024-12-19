6a7d076c0bbb99f1c66fdd83b28c3490c0254457
RedFoxIV <38788538+RedFoxIV@users.noreply.github.com>
2024-12-20 00:25:16 +0300

    Ta zupa była z mlekiem

--- Content.Server/_White/Plumbing/PlumbingSystem.cs
+++ Content.Server/_White/Plumbing/PlumbingSystem.cs
@@ -1,4 +1,11 @@
+using Content.Server.Chemistry.Containers.EntitySystems;
+using Content.Server.NodeContainer;
 using Content.Shared._White.Plumbing;
+using Content.Shared.Chemistry.Components;
+using Content.Shared.Chemistry.Components.SolutionManager;
+using Content.Shared.FixedPoint;
+using Robust.Shared.Containers;
+using Robust.Shared.Timing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,18 +16,62 @@ namespace Content.Server._White.Plumbing;
 
 public sealed class PlumbingSystem : EntitySystem
 {
+
+    [Dependency] private readonly SolutionContainerSystem _solution = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+
+
+    //var simpleDemandQuery = EntityQueryEnumerator<PlumbingSimpleDemandComponent>();
     public override void Initialize()
     {
-        //SubscribeLocalEvent<PlumbingSimpleDemandComponent, PlumbingTickEvent>(SimpleDemandTick);
+        SubscribeLocalEvent<PlumbingFactoryComponent, PlumbingPollOutputsEvent>(OnFactoryPoll);
+        SubscribeLocalEvent < PlumbingFactoryComponent, PlumbingRequestOutputEvent>(OnFactoryRequest);
+        //simpleDemandQuery = GetEntityQuery<PlumbingSimpleDemandComponent>();
     }
 
     public override void Update(float frameTime)
     {
-
+        if((_timing.CurTick.Value % _timing.TickRate) == 0)
+        {
+            var query = EntityQueryEnumerator<PlumbingSimpleDemandComponent>();
+            while(query.MoveNext(out EntityUid uid, out var comp))
+            {
+                SimpleDemandTick(uid, comp);
+            }
+        }
     }
 
+
+    // todo: this is actually stupid
+    // case in point: 3 tanks connected to one factory will all pull the same perTick of
+    //                chemicals instead of everyone getting a third of the output.
+    // move polls over to output nodes, so that they check for available inputs and not the other way around
+    // it just makes more sense if i do not do the whole song and dance ss13 did in order to avoid shit like 10.0000001 water
+    // i can either do a "faithful recreation", or do something that resembles the original in basic functionality.
+    // it's easier to do the latter, especially if we're leaning on the whole "different from ss13" idea.
     private void SimpleDemandTick(EntityUid uid, PlumbingSimpleDemandComponent comp)
     {
+        if (!TryComp<NodeContainerComponent>(uid, out var nodecont))
+            return;
+
+        if (!nodecont.Nodes.TryGetValue("input", out var inputNode))
+            return;
+
+        if (!TryComp<ContainerManagerComponent>(uid, out var contman) || ! _solution.TryGetSolution(uid, comp.Solution, out var _, out var solution))
+            return;
+
+        var ev = new PlumbingPollOutputsEvent();
+        foreach(var output in ((PlumbingNodeGroup) inputNode.NodeGroup!).OutputNodes)
+        {
+            RaiseLocalEvent(output.Owner, ref ev);
+        }
+        var reqev = new PlumbingRequestOutputEvent(comp.RequestPerTick / ev.Valid.Count, solution);
+        foreach (var valid in ev.Valid)
+        {
+            RaiseLocalEvent(valid, reqev);
+        }
+
+
         // trycomp nodecontainer
         // if present, try get input node
         // if present, get nodegroup
@@ -33,6 +84,17 @@ public sealed class PlumbingSystem : EntitySystem
         //      or
         // fuck it
     }
+
+    private void OnFactoryPoll(EntityUid uid, PlumbingFactoryComponent comp, ref PlumbingPollOutputsEvent args)
+    {
+        args.Valid.Add(uid);
+    }
+
+    private void OnFactoryRequest(EntityUid uid, PlumbingFactoryComponent comp, PlumbingRequestOutputEvent args)
+    {
+        args.Solution.AddReagent(comp.reagentprototype, comp.perTick);
+    }
+
 }
 
 
@@ -40,10 +102,19 @@ public sealed class PlumbingSystem : EntitySystem
 public class PlumbingPollOutputsEvent : EntityEventArgs
 {
     public string? ReagentFilter { get; private set; }
-    public List<EntityUid> valid = new();
+    public List<EntityUid> Valid = new();
     public PlumbingPollOutputsEvent(string? filter = null)
     {
         ReagentFilter = filter;
     }
 }
 
+public class PlumbingRequestOutputEvent : EntityEventArgs {
+    public FixedPoint2 Quantity;
+    public Solution Solution;
+    public PlumbingRequestOutputEvent(FixedPoint2 quantity, Solution target)
+    {
+        Quantity = quantity;
+        Solution = target;
+    }
+}
