e169ae090e58742eb96001c842c41a197fffea41
PuroSlavKing <103608145+PuroSlavKing@users.noreply.github.com>
2024-12-30 04:59:59 +0300

    [Port] Codeword Highlighting (#222)
    * Add codeword highlighting (#30092)
    
    * Added codeword highlighting
    
    * Updated to support more codeword roles, color is set serverside
    
    * Review feedback
    
    * Change to a Component-based system using SessionSpecific
    
    * Tidied up CanGetState, set Access restrictions on component
    
    * Clean-up
    
    * Makes the injection ignore brackets, restore some codewords, remove "Taste/Touch" from adjectives
    
    * codewords translate
    
    * one more
    
    ---------
    
    Co-authored-by: SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>

--- Content.Server/GameTicking/Rules/TraitorRuleSystem.cs
+++ Content.Server/GameTicking/Rules/TraitorRuleSystem.cs
@@ -13,8 +13,10 @@ using Content.Shared.Mobs.Systems;
 using Content.Shared.Mood;
 using Content.Shared.Objectives.Components;
 using Content.Shared.PDA;
+using Content.Shared.Radio;
 using Content.Shared.Roles;
 using Content.Shared.Roles.Jobs;
+using Content.Shared.Roles.RoleCodeword;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 using System.Linq;
@@ -24,6 +26,8 @@ namespace Content.Server.GameTicking.Rules;
 
 public sealed class TraitorRuleSystem : GameRuleSystem<TraitorRuleComponent>
 {
+    private static readonly Color TraitorCodewordColor = Color.FromHex("#cc3b3b");
+
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly NpcFactionSystem _npcFaction = default!;
@@ -36,6 +40,7 @@ public sealed class TraitorRuleSystem : GameRuleSystem<TraitorRuleComponent>
     [Dependency] private readonly MobStateSystem _mobStateSystem = default!; // WD EDIT
 
     public const int MaxPicks = 20;
+    [Dependency] private readonly SharedRoleCodewordSystem _roleCodewordSystem = default!;
 
     public override void Initialize()
     {
@@ -142,6 +147,15 @@ public sealed class TraitorRuleSystem : GameRuleSystem<TraitorRuleComponent>
                 Log.Debug($"Added objective {ToPrettyString(objective):objective} with {adding} difficulty");
             }
         }
+        // Send codewords to only the traitor client
+        var color = TraitorCodewordColor; // Fall back to a dark red Syndicate color if a prototype is not found
+
+        RoleCodewordComponent codewordComp = EnsureComp<RoleCodewordComponent>(mindId);
+        _roleCodewordSystem.SetRoleCodewords(codewordComp, "traitor", component.Codewords.ToList(), color);
+
+        // Change the faction
+        _npcFaction.RemoveFaction(traitor, component.NanoTrasenFaction, false);
+        _npcFaction.AddFaction(traitor, component.SyndicateFaction);
 
         return true;
     }
