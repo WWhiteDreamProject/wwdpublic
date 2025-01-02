a1e067a85ad8bc902f17cd7aff9dd15111cfa1f6
RedFoxIV <38788538+RedFoxIV@users.noreply.github.com>
2025-01-02 18:06:08 +0300

    fucking

--- Content.Server/_White/AspectsSystem/Aspects/RulonOboevAspect.cs
+++ Content.Server/_White/AspectsSystem/Aspects/RulonOboevAspect.cs
@@ -35,22 +35,22 @@ public sealed class RulonOboevAspect : AspectSystem<RenameCrewAspectComponent>
     protected override void Ended(EntityUid uid, RenameCrewAspectComponent component, GameRuleComponent gameRule, GameRuleEndedEvent args)
     {
         base.Ended(uid, component, gameRule, args);
-        existing = null;
+        _existing = null;
     }
 
     public override void Shutdown()
     {
         base.Shutdown();
-        existing = null;
+        _existing = null;
     }
 
     private void OnCompInit(EntityUid uid, RenameCrewAspectComponent comp, ComponentInit args)
     {
-        if (existing.HasValue) {
+        if (_existing.HasValue) {
             //Sawmill.Error("RulonOboev aspect should only exist as a single gamerule. Stop spamming shit.");
             return; // fuck off we're full
         }
-        existing = uid;
+        _existing = uid;
         for (int i = 0; i < comp.FirstNames.Count; i++)
         {
             for (int j = 0; j < comp.LastNames.Count; j++)
@@ -84,10 +84,10 @@ public sealed class RulonOboevAspect : AspectSystem<RenameCrewAspectComponent>
     /// </summary>
     private void OnPlayerSpawnComplete(PlayerSpawnCompleteEvent args)
     {
-        if (!existing.HasValue)
+        if (!_existing.HasValue)
             return;
 
-        var comp = Comp<RenameCrewAspectComponent>(existing.Value);
+        var comp = Comp<RenameCrewAspectComponent>(_existing.Value);
 
         EntityUid entityUid = args.Mob;
 
