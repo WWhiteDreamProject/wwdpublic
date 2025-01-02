0de153797d174c1d63678648944626ec7f05a7a1
RedFoxIV <38788538+RedFoxIV@users.noreply.github.com>
2025-01-02 18:12:03 +0300

    Enter commit message

--- Content.Server/_White/AspectsSystem/Aspects/RulonOboevAspect.cs
+++ Content.Server/_White/AspectsSystem/Aspects/RulonOboevAspect.cs
@@ -5,11 +5,13 @@ using Content.Server.Administration.Systems;
 using Content.Server.GameTicking;
 using Content.Server.PDA;
 using Content.Server.StationRecords.Systems;
+using Content.Shared.Access.Components;
 using Content.Shared.GameTicking.Components;
 using Content.Shared.Mind;
 using Content.Shared.PDA;
 using Content.Shared.StationRecords;
 using Robust.Server.Player;
+using Robust.Shared.Console;
 using Robust.Shared.Player;
 using Robust.Shared.Random;
 using Robust.Shared.Utility;
@@ -21,6 +23,7 @@ public sealed class RulonOboevAspect : AspectSystem<RenameCrewAspectComponent>
 {
     [Dependency] private readonly IEntityManager _entManager = default!;
     [Dependency] private readonly IPlayerManager _playerManager = default!;
+    [Dependency] private readonly IConsoleHost _con = default!;
     [Dependency] private readonly IRobustRandom _rng = default!;
 
     private EntityUid? _existing = null;
@@ -65,6 +68,7 @@ public sealed class RulonOboevAspect : AspectSystem<RenameCrewAspectComponent>
         if (_rng.Prob(comp.SpecialNameChance) && comp.SpecialNames.Count > 0)
         {
             name = _rng.PickAndTake(comp.SpecialNames);
+            DebugTools.Assert(name.Length < IdCardConsoleComponent.MaxFullNameLength);
             return true;
         }
 
@@ -72,6 +76,7 @@ public sealed class RulonOboevAspect : AspectSystem<RenameCrewAspectComponent>
         {
             (int first, int last) = _rng.PickAndTake(comp.RegularNameCombos);
             name = string.Join(' ', comp.FirstNames[first], comp.LastNames[last]);
+            DebugTools.Assert(name.Length < IdCardConsoleComponent.MaxFullNameLength);
             return true;
         }
 
@@ -88,68 +93,10 @@ public sealed class RulonOboevAspect : AspectSystem<RenameCrewAspectComponent>
             return;
 
         var comp = Comp<RenameCrewAspectComponent>(_existing.Value);
-
-        EntityUid entityUid = args.Mob;
-
-        if (!PickName(comp, out var name))
+        if (!PickName(comp, out var newName))
             return;
 
-        // Metadata
-        var metadata = _entManager.GetComponent<MetaDataComponent>(entityUid);
-        var oldName = metadata.EntityName;
-        _entManager.System<MetaDataSystem>().SetEntityName(entityUid, name, metadata);
-
-        var minds = _entManager.System<SharedMindSystem>();
-
-        if (minds.TryGetMind(entityUid, out var mindId, out var mind))
-        {
-            // Mind
-            mind.CharacterName = name;
-            _entManager.Dirty(mindId, mind);
-        }
-
-        // Id Cards
-        if (_entManager.TrySystem<IdCardSystem>(out var idCardSystem))
-        {
-            if (idCardSystem.TryFindIdCard(entityUid, out var idCard))
-            {
-                idCardSystem.TryChangeFullName(idCard, name, idCard);
-
-                // Records
-                // This is done here because ID cards are linked to station records
-                if (_entManager.TrySystem<StationRecordsSystem>(out var recordsSystem)
-                    && _entManager.TryGetComponent(idCard, out StationRecordKeyStorageComponent? keyStorage)
-                    && keyStorage.Key is { } key)
-                {
-                    if (recordsSystem.TryGetRecord<GeneralStationRecord>(key, out var generalRecord))
-                    {
-                        generalRecord.Name = name;
-                    }
-
-                    recordsSystem.Synchronize(key);
-                }
-            }
-        }
-
-        // PDAs
-        if (_entManager.TrySystem<PdaSystem>(out var pdaSystem))
-        {
-            var query = _entManager.EntityQueryEnumerator<PdaComponent>();
-            while (query.MoveNext(out var uid, out var pda))
-            {
-                if (pda.OwnerName == oldName)
-                {
-                    pdaSystem.SetOwner(uid, pda, name);
-                }
-            }
-        }
-
-        // Admin Overlay
-        if (_entManager.TrySystem<AdminSystem>(out var adminSystem)
-            && _entManager.TryGetComponent<ActorComponent>(entityUid, out var actorComp))
-        {
-            adminSystem.UpdatePlayerList(actorComp.PlayerSession);
-        }
+        _con.ExecuteCommand($"rename {args.Mob} {newName}");
     }
 }
 
