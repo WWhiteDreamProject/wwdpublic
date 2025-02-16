076ec43c1e9d9b2e6d1e1635ec54073d3a253e72
Simon <63975668+simyon264@users.noreply.github.com>
2025-02-16 12:06:13 +0300

    Add ability for admins to change certain cvars via command. (#35105)
    * Add ability for admins to change certain cvars via command.
    
    * Cleanup
    
    * More cleanup.
    
    * Even more cleanup.
    
    * WAITER! WAITER! ONE MORE COMMIT PLEASE!
    
    * Remove requirement for Admin AdminFlag
    
    Not needed as the command checks for the perms.
    
    * Add search function to CVars and help text
    
    * Move to controller instead.
    
    * Add another cvar for testing
    
    * Remove unused comment
    
    * Move to Post Server Initialize
    
    LoC does not work at the earlier stages of server initalization
    
    * Remove unneeded comment
    
    We clear out list so its no longer needed
    
    (cherry picked from commit 29029d2586a0937a2c04ed0421189110a97a3c6a)

--- Content.Server/IoC/ServerContentIoC.cs
+++ Content.Server/IoC/ServerContentIoC.cs
@@ -83,6 +83,7 @@ namespace Content.Server.IoC
             IoCManager.Register<IWatchlistWebhookManager, WatchlistWebhookManager>();
             IoCManager.Register<ConnectionManager>();
             IoCManager.Register<MultiServerKickManager>();
+            IoCManager.Register<CVarControlManager>();
             IoCManager.Register<TTSManager>(); // WD EDIT
         }
     }
