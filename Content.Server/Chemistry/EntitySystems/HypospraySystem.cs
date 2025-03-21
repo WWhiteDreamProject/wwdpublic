e4c3993afd1fdceb7a9f90e48284461f39ded636
Eagle-0 <114363363+eagle-0@users.noreply.github.com>
2025-03-21 03:13:27 +0300

    Security Buffs But Real (#2022)
    # Description
    Ports Security buffs from
    [/Goob-Station#953](https://github.com/Goob-Station/Goob-Station/pull/953)
    
    ## Once Again, I am John Combat
    I don't have the energy today for the witty PR jokes. Apologies, people.
    
    # TODO
    * [x] Oil floats on water
    * [x] Cover yourself in oil
    * [x] Wait for it to rain
    * [x] :trollface:
    
    # Media
    
    # Changelog
    🆑 Eagle
    
    * add: Flashbangs now stun and knockdown people. Wear over-ear headset
    or combat hardsuit helmet to reduce flashbang effective range.
     * add: All sec and command members received over-ear headsets.
    * add: Wearing security, ert, syndie or deathsquad gas mask blocks smoke
    inhalation even if internals are off.
     * tweak: Epinephrine now speeds up by 10%.
    * add: Jackboots, combat and mercenary boots now reduce all stun and
    knockdown times by 20%.
     * add: Combat hardsuit helmets now protect from flashes.
     * tweak: Tear gas is now stronger.
     * tweak: Stamcrit stun time is once again 6 seconds.
     * tweak: Security hardsuits now have heat resistance.
    
    ---------
    
    Signed-off-by: VMSolidus <evilexecutive@gmail.com>
    Co-authored-by: Aviu00 <93730715+Aviu00@users.noreply.github.com>
    Co-authored-by: VMSolidus <evilexecutive@gmail.com>
    
    (cherry picked from commit 9bae47bc6d3e44fba8b86e18c78e6b0f3fe00ed6)

--- Content.Server/Chemistry/EntitySystems/HypospraySystem.cs
+++ Content.Server/Chemistry/EntitySystems/HypospraySystem.cs
@@ -75,7 +75,7 @@ public sealed class HypospraySystem : SharedHypospraySystem
 
     public void OnAttack(Entity<HyposprayComponent> entity, ref MeleeHitEvent args)
     {
-        if (args.Handled) // WD EDIT
+        if (args.Handled) // Goobstation
             return;
 
         if (!args.HitEntities.Any())
