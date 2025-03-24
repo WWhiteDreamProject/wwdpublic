bfea9da5ea9e7a679046ca42a17473781c6dcf24
hskveez <neyron.feed@mail.ru>
2025-03-24 23:06:58 +0500

    Я себя зарежу нахуй за такое

--- Content.Server/_White/RadialEntityMorph/RadialEntityMorphSystem.cs
+++ Content.Server/_White/RadialEntityMorph/RadialEntityMorphSystem.cs
@@ -1,10 +1,52 @@
+using Content.Server.Hands.Systems;
+using Content.Shared.Hands.Components;
 using Content.Shared.RadialSelector;
+using Content.Shared.UserInterface;
+using Robust.Server.GameObjects;
 
 namespace Content.Server._White.RadialEntityMorph;
 
-[RegisterComponent]
-public sealed partial class RadialEntityMorphComponent : Component
+public sealed class RadialEntityMorphSystem : EntitySystem
 {
-    [DataField(required: true)]
-    public List<RadialSelectorEntry> Entries = new();
+    [Dependency] private readonly HandsSystem _hands = default!;
+    [Dependency] private readonly UserInterfaceSystem _ui = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<RadialEntityMorphComponent, BeforeActivatableUIOpenEvent>(OnUseInHand);
+        SubscribeLocalEvent<RadialEntityMorphComponent, RadialSelectorSelectedMessage>(OnPrototypeSelected);
+    }
+
+    private void OnUseInHand(Entity<RadialEntityMorphComponent> entity, ref BeforeActivatableUIOpenEvent args)
+    {
+        if (!_ui.HasUi(entity.Owner, RadialSelectorUiKey.Key))
+            return;
+
+        _ui.SetUiState(entity.Owner, RadialSelectorUiKey.Key, new RadialSelectorState(entity.Comp.Entries));
+    }
+
+
+    private void OnPrototypeSelected(EntityUid uid, RadialEntityMorphComponent component, RadialSelectorSelectedMessage args)
+    {
+        if (args.UiKey is not RadialSelectorUiKey.Key)
+            return;
+
+        if (!TryComp(args.Actor, out HandsComponent? hands))
+            return;
+
+        foreach (var hand in hands.Hands.Values)
+        {
+            if (hand.HeldEntity != uid)
+                continue;
+
+            Del(uid);
+
+            var newItem = Spawn(args.SelectedItem, Transform(args.Actor).Coordinates);
+            _hands.TryPickup(args.Actor, newItem, hand.Name, handsComp: hands);
+
+            _ui.CloseUi(uid, RadialSelectorUiKey.Key);
+            return;
+        }
+    }
 }
