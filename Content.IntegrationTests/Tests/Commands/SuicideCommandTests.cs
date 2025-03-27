31bb8cdd9800b0daa4807c4b87812b5a9bc5e317
Spatison <137375981+Spatison@users.noreply.github.com>
2025-03-27 07:58:09 +0300

    EnvironmentalSuicide

--- Content.IntegrationTests/Tests/Commands/SuicideCommandTests.cs
+++ Content.IntegrationTests/Tests/Commands/SuicideCommandTests.cs
@@ -363,7 +363,7 @@ public sealed class SuicideCommandTests
                     Assert.That(mobStateSystem.IsDead(player, mobStateComp));
                     Assert.That(entManager.TryGetComponent<GhostComponent>(mindComponent.CurrentEntity, out var ghostComp) &&
                                 !ghostComp.CanReturnToBody);
-                    Assert.That(damageableComp.Damage.DamageDict["Blunt"], Is.EqualTo(lethalDamageThreshold / 2)); // WD EDIT
+                    Assert.That(damageableComp.Damage.DamageDict["Slash"], Is.EqualTo(lethalDamageThreshold / 2));
                 });
         });
 
