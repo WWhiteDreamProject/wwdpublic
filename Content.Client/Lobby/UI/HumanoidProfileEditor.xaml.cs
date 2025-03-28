e3cc62ef8c5e3bb478462ad39af7db1b1dbc4d2b
Timfa <timfalken@hotmail.com>
2025-03-28 20:10:00 +0300

    Contractors V1 (#2030)
    <!--
    This is a semi-strict format, you can add/remove sections as needed but
    the order/format should be kept the same
    Remove these comments before submitting
    -->
    
    # Description
    
    <!--
    Explain this PR in as much detail as applicable
    
    Some example prompts to consider:
    How might this affect the game? The codebase?
    What might be some alternatives to this?
    How/Who does this benefit/hurt [the game/codebase]?
    -->
    
    Contractors v1 is the first version of a system that will allow you to
    set a nationality and an employer to your character. Initially, this
    will determine the availability of some loadout items, jobs and traits,
    but this is built to expand upon later.
    
    As of this moment, the PR will let you select a nationality, an employer
    and a lifepath. Nationalities give you a language and a passport, while
    the other two don't do a lot yet. (except Command can only be NanoTrasen
    and most other jobs can't be unemployed)
    
    The passport functions, and the CharacterRequirements do as well.
    
    there's still a lot more that can be done--tying jobs to certain
    employers, items or traits to nationalities or lifepaths, but the reason
    I want to merge it before that's done is primarily so that I don't need
    to be the sole person working on it anymore. The C# is done, the rest is
    YAML, and we have a bunch of competent YAML warriors who will do great
    stuff with this, no doubt.
    
    ---
    
    # TODO
    
    <!--
    A list of everything you have to do before this PR is "complete"
    You probably won't have to complete everything before merging but it's
    good to leave future references
    -->
    
    - [x] Create Nationality and Employer Prototype code
    - [x] Create Nationality and Employer Character Requirements
    - [x] Add both fields to the Character Creation menu
    - [x] Create Nationality and Employer Prototypes
    - [x] Create Nationality and Employer Prototypes YAML
    - [x] Add requirements to a few jobs
    - [ ] Add requirements to a few traits
    - [ ] Add requirements to a few loadout items
    - [x] create a passport item that can be opened and closed
    - [x] Give each nationality a passport item valid for their species
    
    ---
    
    <!--
    This is default collapsed, readers click to expand it and see all your
    media
    The PR media section can get very large at times, so this is a good way
    to keep it clean
    The title is written using HTML tags
    The title must be within the <summary> tags or you won't see it
    -->
    
    <details><summary><h1>Media</h1></summary>
    <p>
    
    ![image](https://github.com/user-attachments/assets/9c44cf11-766b-4368-af69-8655048e992a)
    
    https://github.com/user-attachments/assets/9e61aed8-2e07-4d44-89c7-595a170df8c7
    
    </p>
    </details>
    
    ---
    
    # Changelog
    
    <!--
    You can add an author after the `:cl:` to change the name that appears
    in the changelog (ex: `:cl: Death`)
    Leaving it blank will default to your GitHub display name
    This includes all available types for the changelog
    -->
    
    :cl:
    - add: Contractors. Be sure to edit your character to set an employer
    and nationality!
    - add: Nationality, Lifepath, and Employer have been added to character
    creation. These don't currently do much except spawn a passport
    containing information about where your character is from and who they
    are, but they are fully integrated with Character Requirements, and by
    extension can be used for and alongside Traits, Loadouts, Jobs,
    Antagonists, etc.
    
    ---------
    
    Signed-off-by: Timfa <timfalken@hotmail.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Co-authored-by: VMSolidus <evilexecutive@gmail.com>
    
    (cherry picked from commit c3b12c62ee3bd226b57022690d1b24cb7cf54e3f)

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -8,6 +8,7 @@ using Content.Client.Message;
 using Content.Client.Players.PlayTimeTracking;
 using Content.Client.UserInterface.Controls;
 using Content.Client.UserInterface.Systems.Guidebook;
+using Content.Shared._EE.Contractors.Prototypes;
 using Content.Shared._White.Humanoid.Prototypes;
 using Content.Shared.CCVar;
 using Content.Shared.Clothing.Components;
@@ -78,6 +79,11 @@ namespace Content.Client.Lobby.UI
 
         private List<SpeciesPrototype> _species = new();
         private List<BodyTypePrototype> _bodyTypes = new(); // WD EDIT
+        // EE - Contractor System Changes Start
+        private List<NationalityPrototype> _nationalies = new();
+        private List<EmployerPrototype> _employers = new();
+        private List<LifepathPrototype> _lifepaths = new();
+        // EE - Contractor System Changes End
         private List<(string, RequirementsSelector)> _jobPriorities = new();
         private readonly Dictionary<string, BoxContainer> _jobCategories;
 
@@ -268,6 +274,32 @@ namespace Content.Client.Lobby.UI
             };
 
             #endregion Species
+
+            #region Contractors
+
+            RefreshNationalities();
+            RefreshEmployers();
+            RefreshLifepaths();
+
+            NationalityButton.OnItemSelected += args =>
+            {
+                NationalityButton.SelectId(args.Id);
+                SetNationality(_nationalies[args.Id].ID);
+            };
+
+            EmployerButton.OnItemSelected += args =>
+            {
+                EmployerButton.SelectId(args.Id);
+                SetEmployer(_employers[args.Id].ID);
+            };
+
+            LifepathButton.OnItemSelected += args =>
+            {
+                LifepathButton.SelectId(args.Id);
+                SetLifepath(_lifepaths[args.Id].ID);
+            };
+
+            #endregion Contractors
 
             #region Height and Width
 
@@ -626,6 +658,99 @@ namespace Content.Client.Lobby.UI
                 SetSpecies(SharedHumanoidAppearanceSystem.DefaultSpecies);
         }
 
+        public void RefreshNationalities()
+        {
+            NationalityButton.Clear();
+            _nationalies.Clear();
+
+            _nationalies.AddRange(_prototypeManager.EnumeratePrototypes<NationalityPrototype>()
+                .Where(o => _characterRequirementsSystem.CheckRequirementsValid(o.Requirements,
+                    _controller.GetPreferredJob(Profile ?? HumanoidCharacterProfile.DefaultWithSpecies()),
+                    Profile ?? HumanoidCharacterProfile.DefaultWithSpecies(),
+                    _requirements.GetRawPlayTimeTrackers(),
+                    _requirements.IsWhitelisted(),
+                    o,
+                    _entManager,
+                    _prototypeManager,
+                    _cfgManager, out _)));
+
+            var nationalityIds = _nationalies.Select(o => o.ID).ToList();
+
+            for (var i = 0; i < _nationalies.Count; i++)
+            {
+                NationalityButton.AddItem(Loc.GetString(_nationalies[i].NameKey), i);
+
+                if (Profile?.Nationality == _nationalies[i].ID)
+                    NationalityButton.SelectId(i);
+            }
+
+            // If our nationality isn't available, reset it to default
+            if (Profile != null && !nationalityIds.Contains(Profile.Nationality))
+                SetNationality(SharedHumanoidAppearanceSystem.DefaultNationality);
+        }
+
+        public void RefreshEmployers()
+        {
+            EmployerButton.Clear();
+            _employers.Clear();
+
+            _employers.AddRange(_prototypeManager.EnumeratePrototypes<EmployerPrototype>()
+                .Where(o => _characterRequirementsSystem.CheckRequirementsValid(o.Requirements,
+                _controller.GetPreferredJob(Profile ?? HumanoidCharacterProfile.DefaultWithSpecies()),
+                Profile ?? HumanoidCharacterProfile.DefaultWithSpecies(),
+                _requirements.GetRawPlayTimeTrackers(),
+                _requirements.IsWhitelisted(),
+                o,
+                _entManager,
+                _prototypeManager,
+                _cfgManager, out _)));
+
+            var employerIds = _employers.Select(o => o.ID).ToList();
+
+            for (var i = 0; i < _employers.Count; i++)
+            {
+                EmployerButton.AddItem(Loc.GetString(_employers[i].NameKey), i);
+
+                if (Profile?.Employer == _employers[i].ID)
+                    EmployerButton.SelectId(i);
+            }
+
+            // If our employer isn't available, reset it to default
+            if (Profile != null && !employerIds.Contains(Profile.Employer))
+                SetEmployer(SharedHumanoidAppearanceSystem.DefaultEmployer);
+        }
+
+        public void RefreshLifepaths()
+        {
+            LifepathButton.Clear();
+            _lifepaths.Clear();
+
+            _lifepaths.AddRange(_prototypeManager.EnumeratePrototypes<LifepathPrototype>()
+                .Where(o => _characterRequirementsSystem.CheckRequirementsValid(o.Requirements,
+                _controller.GetPreferredJob(Profile ?? HumanoidCharacterProfile.DefaultWithSpecies()),
+                Profile ?? HumanoidCharacterProfile.DefaultWithSpecies(),
+                _requirements.GetRawPlayTimeTrackers(),
+                _requirements.IsWhitelisted(),
+                o,
+                _entManager,
+                _prototypeManager,
+                _cfgManager, out _)));
+
+            var lifepathIds = _lifepaths.Select(o => o.ID).ToList();
+
+            for (var i = 0; i < _lifepaths.Count; i++)
+            {
+                LifepathButton.AddItem(Loc.GetString(_lifepaths[i].NameKey), i);
+
+                if (Profile?.Lifepath == _lifepaths[i].ID)
+                    LifepathButton.SelectId(i);
+            }
+
+            // If our lifepath isn't available, reset it to default
+            if (Profile != null && !lifepathIds.Contains(Profile.Lifepath))
+                SetLifepath(SharedHumanoidAppearanceSystem.DefaultLifepath);
+        }
+
         public void RefreshAntags()
         {
             AntagList.DisposeAllChildren();
@@ -770,6 +895,9 @@ namespace Content.Client.Lobby.UI
             RefreshAntags();
             RefreshJobs();
             RefreshSpecies();
+            RefreshNationalities();
+            RefreshEmployers();
+            RefreshLifepaths();
             RefreshFlavorText();
             ReloadPreview();
 
@@ -873,7 +1001,7 @@ namespace Content.Client.Lobby.UI
 
                     category.AddChild(new PanelContainer
                     {
-                        PanelOverride = new StyleBoxFlat { BackgroundColor = Color.FromHex("#464966") },
+                        PanelOverride = new StyleBoxFlat {BackgroundColor = Color.FromHex("#232323")}, // WD EDIT
                         Children =
                         {
                             new Label
@@ -1005,7 +1133,7 @@ namespace Content.Client.Lobby.UI
 
                     category.AddChild(new PanelContainer
                     {
-                        PanelOverride = new StyleBoxFlat {BackgroundColor = Color.FromHex("#232323")}, // WD EDIT
+                        PanelOverride = new StyleBoxFlat {BackgroundColor = Color.FromHex("#464966")},
                         Children =
                         {
                             new Label
@@ -1344,6 +1472,33 @@ namespace Content.Client.Lobby.UI
             IsDirty = true;
             ReloadProfilePreview();
             ReloadClothes(); // Species may have job-specific gear, reload the clothes
+        }
+
+        private void SetNationality(string newNationality)
+        {
+            Profile = Profile?.WithNationality(newNationality);
+            UpdateCharacterRequired();
+            IsDirty = true;
+            ReloadProfilePreview();
+            ReloadClothes(); // Nationalities may have specific gear, reload the clothes
+        }
+
+        private void SetEmployer(string newEmployer)
+        {
+            Profile = Profile?.WithEmployer(newEmployer);
+            UpdateCharacterRequired();
+            IsDirty = true;
+            ReloadProfilePreview();
+            ReloadClothes(); // Employers may have specific gear, reload the clothes
+        }
+
+        private void SetLifepath(string newLifepath)
+        {
+            Profile = Profile?.WithLifepath(newLifepath);
+            UpdateCharacterRequired();
+            IsDirty = true;
+            ReloadProfilePreview();
+            ReloadClothes(); // Lifepaths may have specific gear, reload the clothes
         }
 
         private void SetName(string newName)
@@ -2629,6 +2784,9 @@ namespace Content.Client.Lobby.UI
 
         private void UpdateCharacterRequired()
         {
+            RefreshNationalities();
+            RefreshEmployers();
+            RefreshLifepaths();
             UpdateRoleRequirements();
             UpdateTraits(TraitsShowUnusableButton.Pressed);
             UpdateLoadouts(LoadoutsShowUnusableButton.Pressed);
