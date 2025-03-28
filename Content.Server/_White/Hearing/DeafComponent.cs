ca55c0e669c18f39b47009bccc172f904e1a98be
vanx <61917534+Vaaankas@users.noreply.github.com>
2025-03-29 00:58:52 +0300

    [Fix] Deafness (#384)
    you can almost hear something...
    
    Co-authored-by: vanx <discord@vanxxxx>

--- Content.Server/_White/Hearing/DeafComponent.cs
+++ Content.Server/_White/Hearing/DeafComponent.cs
@@ -1,11 +1,8 @@
-﻿using System.Threading;
-
-
-namespace Content.Server._White.Hearing;
+﻿namespace Content.Server._White.Hearing;
 
 /// <summary>
 /// Changes all incoming chat messages to DeafChatMessage.
-/// Added by the DeafnessSystem on the HearingChangedEvent
+/// Added by the DeafnessSystem
 /// </summary>
 [RegisterComponent]
 public sealed partial class DeafComponent : Component
@@ -14,10 +11,5 @@ public sealed partial class DeafComponent : Component
     public LocId DeafChatMessage = "deaf-chat-message";
 
     [DataField]
-    public bool Permanent = true;
-
-    [DataField]
-    public float Duration = 0f; // In seconds
-
-    public CancellationTokenSource? TokenSource;
+    public bool AlwaysDeaf = false; // Allows to make someone permanently deaf with VV, for admemes
 }
