eeb6475d170d39952d33c47a5694c398ead89684
Kutosss <162154227+Kutosss@users.noreply.github.com>
2025-04-05 23:26:56 +0300

    Русссский акцент ящуров

--- Content.Server/Speech/EntitySystems/LizardAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/LizardAccentSystem.cs
@@ -5,11 +5,24 @@ namespace Content.Server.Speech.EntitySystems;
 
 public sealed class LizardAccentSystem : EntitySystem
 {
+    // English letters
     private static readonly Regex RegexLowerS = new("s+");
     private static readonly Regex RegexUpperS = new("S+");
     private static readonly Regex RegexInternalX = new(@"(\w)x");
     private static readonly Regex RegexLowerEndX = new(@"\bx([\-|r|R]|\b)");
     private static readonly Regex RegexUpperEndX = new(@"\bX([\-|r|R]|\b)");
+    
+    // Russian letters
+    private static readonly Regex RegexLowerZ = new("з");
+    private static readonly Regex RegexUpperZ = new("З");
+    private static readonly Regex RegexLowerS_Ru = new("с");
+    private static readonly Regex RegexUpperS_Ru = new("С");
+    private static readonly Regex RegexLowerSh = new("ш");
+    private static readonly Regex RegexUpperSh = new("Ш");
+    private static readonly Regex RegexLowerSch = new("щ");
+    private static readonly Regex RegexUpperSch = new("Щ");
+    private static readonly Regex RegexLowerTs = new("ц");
+    private static readonly Regex RegexUpperTs = new("Ц");
 
     public override void Initialize()
     {
@@ -21,6 +34,7 @@ public sealed class LizardAccentSystem : EntitySystem
     {
         var message = args.Message;
 
+        // English replacements
         // hissss
         message = RegexLowerS.Replace(message, "sss");
         // hiSSS
@@ -31,6 +45,23 @@ public sealed class LizardAccentSystem : EntitySystem
         message = RegexLowerEndX.Replace(message, "ecks$1");
         // eckS
         message = RegexUpperEndX.Replace(message, "ECKS$1");
+
+        // Russian replacements
+        // З -> С
+        message = RegexLowerZ.Replace(message, "с");
+        message = RegexUpperZ.Replace(message, "С");
+        // Растягиваем С
+        message = RegexLowerS_Ru.Replace(message, "ссс");
+        message = RegexUpperS_Ru.Replace(message, "ССС");
+        // Растягиваем Ш
+        message = RegexLowerSh.Replace(message, "шшш");
+        message = RegexUpperSh.Replace(message, "ШШШ");
+        // Растягиваем Щ
+        message = RegexLowerSch.Replace(message, "щщщ");
+        message = RegexUpperSch.Replace(message, "ЩЩЩ");
+        // Ц -> СС
+        message = RegexLowerTs.Replace(message, "сс");
+        message = RegexUpperTs.Replace(message, "СС");
 
         args.Message = message;
     }
