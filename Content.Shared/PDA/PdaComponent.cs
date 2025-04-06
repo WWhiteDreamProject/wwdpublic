5a3ddf4b13a458a3e6d33328734593d861622d1a
Kutosss <162154227+Kutosss@users.noreply.github.com>
2025-04-06 21:45:15 +0300

    Apply suggestions from code reviegfdw
    gfd
    
    Co-authored-by: Spatison <137375981+Spatison@users.noreply.github.com>

--- Content.Shared/PDA/PdaComponent.cs
+++ Content.Shared/PDA/PdaComponent.cs
@@ -12,7 +12,7 @@ namespace Content.Shared.PDA
         public const string PdaIdSlotId = "PDA-id";
         public const string PdaPenSlotId = "PDA-pen";
         public const string PdaPaiSlotId = "PDA-pai";
-        public const string PdaPassportSlotId = "PDA-passport";
+        public const string PdaPassportSlotId = "PDA-passport"; // WD EDIT
 
         /// <summary>
         /// The base PDA sprite state, eg. "pda", "pda-clown"
@@ -29,8 +29,10 @@ namespace Content.Shared.PDA
         [DataField("paiSlot")]
         public ItemSlot PaiSlot = new();
 
+        // WD EDIT START
         [DataField("passportSlot")]
         public ItemSlot PassportSlot = new();
+        // WD EDIT END
 
         // Really this should just be using ItemSlot.StartingItem. However, seeing as we have so many different starting
         // PDA's and no nice way to inherit the other fields from the ItemSlot data definition, this makes the yaml much
