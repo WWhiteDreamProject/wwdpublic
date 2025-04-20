1e363c7346dbaf22803da7386a07c4143dd6ae12
Remuchi <RemuchiOfficial@gmail.com>
2025-04-20 11:15:45 +0700

    Revert "Merge pull request #462 from WWhiteDreamProject/revert-460-upstream"
    This reverts commit 092dba42e54099e8d7b653cd962e03dd882c0acb, reversing
    changes made to e6315c2320bf522ae866dc77eceb20c66c03a5fb.

--- Content.Server.Database/Migrations/Postgres/PostgresServerDbContextModelSnapshot.cs
+++ Content.Server.Database/Migrations/Postgres/PostgresServerDbContextModelSnapshot.cs
@@ -839,6 +839,11 @@ namespace Content.Server.Database.Migrations.Postgres
                         .HasColumnType("text")
                         .HasColumnName("backpack");
 
+                    b.Property<string>("BodyType")
+                        .IsRequired()
+                        .HasColumnType("text")
+                        .HasColumnName("body_type");
+
                     b.Property<string>("CharacterName")
                         .IsRequired()
                         .HasColumnType("text")
@@ -848,6 +853,10 @@ namespace Content.Server.Database.Migrations.Postgres
                         .IsRequired()
                         .HasColumnType("text")
                         .HasColumnName("clothing");
+
+                    b.Property<string>("ClownName")
+                        .HasColumnType("TEXT")
+                        .HasColumnName("clown_name");
 
                     b.Property<string>("CustomSpecieName")
                         .IsRequired()
@@ -933,18 +942,6 @@ namespace Content.Server.Database.Migrations.Postgres
                         .HasColumnType("text")
                         .HasColumnName("sex");
 
-                    // WD EDIT START
-                    b.Property<string>("Voice")
-                        .IsRequired()
-                        .HasColumnType("text")
-                        .HasColumnName("voice");
-
-                    b.Property<string>("BodyType")
-                        .IsRequired()
-                        .HasColumnType("text")
-                        .HasColumnName("body_type");
-                    // WD EDIT END
-
                     b.Property<string>("SkinColor")
                         .IsRequired()
                         .HasColumnType("text")
@@ -966,6 +963,11 @@ namespace Content.Server.Database.Migrations.Postgres
                     b.Property<string>("StationAiName")
                         .HasColumnType("text")
                         .HasColumnName("station_ai_name");
+
+                    b.Property<string>("Voice")
+                        .IsRequired()
+                        .HasColumnType("text")
+                        .HasColumnName("voice");
 
                     b.Property<float>("Width")
                         .HasColumnType("real")
