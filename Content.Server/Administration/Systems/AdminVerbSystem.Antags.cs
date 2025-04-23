71472a58ba21c9a76b5ec9b2507298284d2e5fae
Kai5 <68296202+Kai518@users.noreply.github.com>
2025-04-24 02:29:30 +0300

    Прикольное

--- Content.Server/Administration/Systems/AdminVerbSystem.Antags.cs
+++ Content.Server/Administration/Systems/AdminVerbSystem.Antags.cs
@@ -1,3 +1,4 @@
+using Content.Goobstation.Common.Blob;
 using Content.Server.Administration.Commands;
 using Content.Server.Antag;
 using Content.Server.GameTicking.Rules.Components;
@@ -173,5 +174,21 @@ public sealed partial class AdminVerbSystem
             Message = Loc.GetString("admin-verb-make-blood-cultist"),
         };
         args.Verbs.Add(cultist);
+
+        // Goobstation - Blob
+        Verb blobAntag = new()
+        {
+            Text = Loc.GetString("admin-verb-text-make-blob"),
+            Category = VerbCategory.Antag,
+            Icon = new SpriteSpecifier.Rsi(new("/Textures/_Goobstation/Blob/Actions/blob.rsi"), "blobFactory"),
+            Act = () =>
+            {
+                EnsureComp<BlobCarrierComponent>(args.Target).HasMind = HasComp<ActorComponent>(args.Target);
+            },
+            Impact = LogImpact.High,
+            Message = Loc.GetString("admin-verb-text-make-blob"),
+        };
+        if (!HasComp<SiliconComponent>(args.Target))
+            args.Verbs.Add(blobAntag);
     }
 }
