6e0a1b4753a04e06f1381848df66789ed0512031
Boevi4ok <156510964+89Eblan@users.noreply.github.com>
2025-05-05 23:48:23 +0300

    Update Content.Client/Inventory/StrippableBoundUserInterface.cs
    Co-authored-by: Spatison <137375981+Spatison@users.noreply.github.com>

--- Content.Client/Inventory/StrippableBoundUserInterface.cs
+++ Content.Client/Inventory/StrippableBoundUserInterface.cs
@@ -229,8 +229,7 @@ namespace Content.Client.Inventory
             // Hovering, highlighting & storage are features of general hands & inv GUIs. This UI just re-uses these because I'm lazy.
             button.ClearHover();
             button.StorageButton.Visible = false;
-            //WWDP edit start
-            if (entity == null || EntMan.HasComponent<StripMenuHideIconComponent>(entity.Value))
+            if (entity == null || EntMan.HasComponent<StripMenuHideIconComponent>(entity.Value)) // WD EDIT
             {
                 button.SetEntity(null);
                 return;
