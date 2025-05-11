35c394be7e663363bb15a2a68cd82b3f50c0db19
Spatison <137375981+Spatison@users.noreply.github.com>
2025-05-11 13:49:52 +0300

    tweak

--- Content.Shared/CCVar/CCVars.Atmos.cs
+++ Content.Shared/CCVar/CCVars.Atmos.cs
@@ -85,7 +85,7 @@ public sealed partial class CCVars
     ///     unless we truncate it somewhere.
     /// </summary>
     public static readonly CVarDef<float> AtmosSpacingMinGas =
-        CVarDef.Create("atmos.mmos_min_gas", 25f, CVar.SERVERONLY); // WD EDIT
+        CVarDef.Create("atmos.mmos_min_gas", 2f, CVar.SERVERONLY);
 
     /// <summary>
     ///     How much wind can go through a single tile before that tile doesn't depressurize itself
