8eed4ab0033473fc2b2632773f0af1e756e82551
DVONIKS <88653131+DVOniksWyvern@users.noreply.github.com>
2025-05-25 18:16:04 +0300

    Revert "Hot reload" (#517)
    Revert "Hot reload (#500)"
    
    This reverts commit c754a645d7cc95ff83470d78e258f52b6c6c5eb5.

--- Content.Client/Weapons/Ranged/Systems/GunSystem.Battery.cs
+++ Content.Client/Weapons/Ranged/Systems/GunSystem.Battery.cs
@@ -1,5 +1,4 @@
 using Content.Shared.Weapons.Ranged.Components;
-using Robust.Shared.Timing;
 
 namespace Content.Client.Weapons.Ranged.Systems;
 
@@ -10,23 +9,22 @@ public sealed partial class GunSystem
         base.InitializeBattery();
         // Hitscan
         SubscribeLocalEvent<HitscanBatteryAmmoProviderComponent, AmmoCounterControlEvent>(OnControl);
-        //SubscribeLocalEvent<HitscanBatteryAmmoProviderComponent, UpdateAmmoCounterEvent>(OnAmmoCountUpdate); // WWDP EDIT
+        SubscribeLocalEvent<HitscanBatteryAmmoProviderComponent, UpdateAmmoCounterEvent>(OnAmmoCountUpdate);
 
         // Projectile
         SubscribeLocalEvent<ProjectileBatteryAmmoProviderComponent, AmmoCounterControlEvent>(OnControl);
-        //SubscribeLocalEvent<ProjectileBatteryAmmoProviderComponent, UpdateAmmoCounterEvent>(OnAmmoCountUpdate); // WWDP EDIT
+        SubscribeLocalEvent<ProjectileBatteryAmmoProviderComponent, UpdateAmmoCounterEvent>(OnAmmoCountUpdate);
     }
 
-    // WWDP EDIT - DEFUNCT - Trying to make gun-related status controls update themselves instead of whatever the fuck is currently going on
-    //private void OnAmmoCountUpdate(EntityUid uid, BatteryAmmoProviderComponent component, UpdateAmmoCounterEvent args)
-    //{
-    //    if (args.Control is not BoxesStatusControl boxes) return;
-    //    
-    //    boxes.Update(component.Shots, component.Capacity);
-    //}
+    private void OnAmmoCountUpdate(EntityUid uid, BatteryAmmoProviderComponent component, UpdateAmmoCounterEvent args)
+    {
+        if (args.Control is not BoxesStatusControl boxes) return;
+
+        boxes.Update(component.Shots, component.Capacity);
+    }
 
     private void OnControl(EntityUid uid, BatteryAmmoProviderComponent component, AmmoCounterControlEvent args)
     {
-        args.Control = new EnergyGunBatteryStatusControl(component); // WWDP EDIT
+        args.Control = new BoxesStatusControl();
     }
 }
