2fd1dd9a51114f7334c0adff55734ada7ad6fb94
RedFoxIV <38788538+RedFoxIV@users.noreply.github.com>
2025-05-27 00:56:11 +0300

    Hot reload 2: Electric Boogaloo (#518)
    * What have i become?
    
    My sweetest metagang
    
    * Everyone i know disconnects
    
    In the end
    
    * And you could have it all
    
    My repo of dirt
    
    * I will let you down
    
    I will make you hurt
    
    * I wear this crown of commits
    
    Upon my liar's branch
    
    * Full of broken code
    
    I cannot compile
    
    * Beneath the stains of time
    
    The comments disappear
    
    * You are someone else
    
    I'm still down here
    
    * What have I become?
    
    My sweetest metafriend
    
    * Everyone i know gets perma'd
    
    In the end
    
    * And you could have it all
    
    My broken repo
    
    * I will let you down
    
    I will get you banned
    
    * If i could start again
    
    A million servers away
    
    * I would keep myself
    
    I would find a way
    
    * ...
    
    * . . .
    
    * Update Resources/Prototypes/DeltaV/Entities/Objects/Weapons/Guns/Battery/battery_guns.yml
    
    Co-authored-by: Spatison <137375981+Spatison@users.noreply.github.com>
    
    * Update Resources/Prototypes/DeltaV/Entities/Objects/Weapons/Guns/Battery/battery_guns.yml
    
    Co-authored-by: Spatison <137375981+Spatison@users.noreply.github.com>
    
    * Update Resources/Prototypes/DeltaV/Entities/Objects/Weapons/Guns/Battery/battery_guns.yml
    
    Co-authored-by: Spatison <137375981+Spatison@users.noreply.github.com>
    
    * Update Resources/Prototypes/DeltaV/Entities/Objects/Weapons/Guns/Battery/battery_guns.yml
    
    Co-authored-by: Spatison <137375981+Spatison@users.noreply.github.com>
    
    * какая удобная фича
    
    Co-authored-by: Spatison <137375981+Spatison@users.noreply.github.com>
    
    * .  .  .
    
    * mnda
    
    Co-authored-by: Spatison <137375981+Spatison@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Boevi4ok <156510964+89Eblan@users.noreply.github.com>
    Co-authored-by: Spatison <137375981+Spatison@users.noreply.github.com>

--- Content.Client/Weapons/Ranged/Systems/GunSystem.AmmoCounter.cs
+++ Content.Client/Weapons/Ranged/Systems/GunSystem.AmmoCounter.cs
@@ -1,14 +1,18 @@
-using System.Numerics;
+using Content.Client._White.Guns;
+using Content.Client._White.UI;
 using Content.Client.IoC;
 using Content.Client.Items;
 using Content.Client.Resources;
 using Content.Client.Stylesheets;
 using Content.Client.Weapons.Ranged.Components;
 using Content.Client.Weapons.Ranged.ItemStatus;
+using Content.Shared._White.Guns;
+using Content.Shared.Weapons.Ranged.Components;
 using Robust.Client.Animations;
 using Robust.Client.Graphics;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
+using System.Numerics;
 
 namespace Content.Client.Weapons.Ranged.Systems;
 
@@ -114,6 +118,7 @@ public sealed partial class GunSystem
         }
     }
 
+    // WWDP - DEFUNCT - Left just in case for upstream compatibility
     public sealed class BoxesStatusControl : Control
     {
         private readonly BatteryBulletRenderer _bullets;
@@ -352,5 +357,149 @@ public sealed partial class GunSystem
         }
     }
 
+    // WWDP EDIT START
+    // here be shitcode
+    public sealed class EnergyGunBatteryStatusControl : Control
+    {
+        private readonly EntityUid _gun;
+        private readonly BarControl _ammoBar;
+        private readonly Label _ammoLabel;
+        private readonly Label _heatLabel;
+        private readonly Label _lampLabel;
+        private readonly BatteryAmmoProviderComponent _ammoProvider;
+        private readonly GunOverheatComponent? _regulator;
+        private readonly GunOverheatSystem _regSys;
+        private readonly IEntityManager _entMan;
+
+        private int _ammoCount = 0;
+        private bool _heatLimitEnabled = true;
+        private float _heatLimit = 0;
+        private float _heat = 0; // caching temperature and ammo counts so that the labels don't end up having their measures invalidated every frame
+                                 // not sure if this makes any difference performance-wise, but it just seems like a good idea
+        public EnergyGunBatteryStatusControl(BatteryAmmoProviderComponent comp)
+        {
+            _entMan = IoCManager.Resolve<IEntityManager>();
+            _regSys = _entMan.System<GunOverheatSystem>();
+            _gun = comp.Owner;
+            _ammoProvider = comp;
+            _ammoCount = comp.Shots;
+            MinHeight = 15;
+            HorizontalExpand = true;
+            VerticalAlignment = Control.VAlignment.Center;
+            AddChild(new BoxContainer // outer box
+            {
+                Orientation = BoxContainer.LayoutOrientation.Vertical,
+                Children =
+                {
+                    new BoxContainer // inner upper box, lamp indicator and temp gauge
+                    {
+                        Orientation = BoxContainer.LayoutOrientation.Horizontal,
+                        Children =
+                        {
+                            (_lampLabel = new Label
+                            {
+                                StyleClasses = { StyleNano.StyleClassItemStatus },
+                                HorizontalAlignment = HAlignment.Left,
+                                VerticalAlignment = VAlignment.Bottom,
+                                Text = $" ●"
+                            }),
+                            (_heatLabel = new Label
+                            {
+                                StyleClasses = { StyleNano.StyleClassItemStatus },
+                                HorizontalAlignment = HAlignment.Right,
+                                HorizontalExpand = true,
+                                VerticalAlignment = VAlignment.Bottom,
+                                Text = $"{_heat-273.15:0.00} °C "
+                            }),
+                        }
+                    },
+                    new BoxContainer // inner lower box, the ammo display and counter
+                    {
+                        Orientation = BoxContainer.LayoutOrientation.Horizontal,
+                        Children =
+                        {
+                            (_ammoBar = new BarControl
+                            {
+                                Rows = 4,
+                                MaxWidth = 75
+                            }),
+                            (_ammoLabel = new Label
+                            {
+                                StyleClasses = { StyleNano.StyleClassItemStatus },
+                                HorizontalExpand = true,
+                                HorizontalAlignment = HAlignment.Right,
+                                VerticalAlignment = VAlignment.Top,
+                                Text = $"x{_ammoCount:00}"
+                            }),
+                        }
+                    }
+                }
+            });
+
+            // if temp regulator component is missing on the gun, hide the temperature gauge and lamp display
+            // since they won't matter anyways
+            if (!_entMan.TryGetComponent(_gun, out _regulator))
+            {
+                _heatLabel.Visible = false;
+                _lampLabel.Visible = false;
+                return;
+            }
+            _lampLabel.Visible = _regulator.RequiresLamp;
+        }
+
+        // still using kelvin because having temperature go from 0 to +inf is much nicer than from -273.15 to +inf
+        private void UpdateTemp(float K)
+        {
+            float celcius = K - 273.15f;
+            // we assume _regulator is not null since we'll check for it before calling this method
+            float maxTemp = _regulator!.MaxDisplayTemperatureCelcius;
+            string currentTemp = celcius > maxTemp ? $"{maxTemp:0}+°C" : $"{celcius:0} °C";
+            if (_regulator.SafetyEnabled)
+                _heatLabel.Text = $"{currentTemp}/{_regulator.TemperatureLimit - 273.15f:0} °C "; // MathF.Min to conserve a single digit for space in an otherwise overly cramped piece of UI.
+            else
+                _heatLabel.Text = currentTemp;
+
+            float hue = 0; // full red
+            const float hueoffset = 0.07f; // raises the 0K color from dark blue to a brighter tone
+
+            if (K < _regulator.TemperatureLimit)
+                hue = 0.66f - ((K) / (_regulator.TemperatureLimit) * 0.55f * (1f - hueoffset) + hueoffset);
+
+            var tempColor = Color.FromHsv(new Robust.Shared.Maths.Vector4(hue, 1, 1, 1));
+            _heatLabel.FontColorOverride = tempColor;
+            _lampLabel.FontColorOverride = tempColor;
+        }
+
+        protected override void PreRenderChildren(ref ControlRenderArguments args)
+        {
+            _ammoBar.Fill = 0;
+            if (_ammoProvider.Capacity > 0)
+                _ammoBar.Fill = (float) _ammoProvider.Shots / _ammoProvider.Capacity;
+
+            if (_ammoCount != _ammoProvider.Shots)
+            {
+                _ammoCount = _ammoProvider.Shots;
+                _ammoLabel.Text = $"x{_ammoCount:00}";
+            }
+
+            // skip all the temperature stuff if the related component is not present;
+            if (_regulator is null)
+                return;
+
+            if (_regSys.GetLamp(_gun, out var lampComp, _regulator))
+            {
+                _lampLabel.Text = lampComp is null || !lampComp.Intact ? " ◌" : " ●";
+            }
+
+            if (_heat != _regulator.CurrentTemperature || _heatLimit != _regulator.TemperatureLimit || _heatLimitEnabled != _regulator.SafetyEnabled)
+            {
+                _heatLimit = _regulator.TemperatureLimit;
+                _heat = _regulator.CurrentTemperature;
+                _heatLimitEnabled = _regulator.SafetyEnabled;
+                UpdateTemp(_heat);
+            }
+        }
+    }
+    // WWDP EDIT END
     #endregion
 }
