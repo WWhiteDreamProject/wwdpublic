4de68a4f4a553a4b4fd34744e9d512974d6c31d4
RedFoxIV <38788538+RedFoxIV@users.noreply.github.com>
2025-06-11 11:06:04 +0300

    changeling deez nuts (#557)
    * no fun allowed
    
    * халявы не будет
    
    * ноу фан эллауд

--- Content.Shared/_Goobstation/Changeling/Changeling.DoAfter.cs
+++ Content.Shared/_Goobstation/Changeling/Changeling.DoAfter.cs
@@ -3,8 +3,14 @@ using Robust.Shared.Serialization;
 
 namespace Content.Shared.Changeling;
 
+// WWDP EDIT START
 [Serializable, NetSerializable]
-public sealed partial class AbsorbDNADoAfterEvent : SimpleDoAfterEvent { }
+public sealed partial class AbsorbDNADoAfterFirstEvent : SimpleDoAfterEvent { }     // le extend
+[Serializable, NetSerializable]
+public sealed partial class AbsorbDNADoAfterSecondEvent : SimpleDoAfterEvent { }    // le stab
+[Serializable, NetSerializable]
+public sealed partial class AbsorbDNADoAfterThirdEvent : SimpleDoAfterEvent { }     // le suck
+// WWDP EDIT END
 
 [Serializable, NetSerializable]
 public sealed partial class ChangelingInfectTargetDoAfterEvent : SimpleDoAfterEvent { }
