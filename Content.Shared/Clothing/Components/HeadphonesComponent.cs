ede6b4f399aeaf28cc745f7d709b4f1b8814cf31
Lachklen <siuxiny360.180@gmail.com>
2025-06-30 06:51:11 +0300

    Добавлено HeadphonesComponent и HeadphonesSystem для переключения спрайтов злосчастным equippedPrefix - Добавлены HeadphonesComponent и HeadphonesSystem для переключения equippedPrefix специально под наушники - Обновлен прототип наушников для использования нового компонента - Добавлены отдельные спрайты inhand для правильной настройки параметров включения/выключения в руке

---
+++ Content.Shared/Clothing/Components/HeadphonesComponent.cs
@@ -0,0 +1,8 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Clothing.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class HeadphonesComponent : Component
+{
+}
