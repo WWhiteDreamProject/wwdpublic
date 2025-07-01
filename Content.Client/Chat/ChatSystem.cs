694904b92619137e820518910a7222a4d28b27a4
Dranik <63515142+potatadranik1984@users.noreply.github.com>
2025-07-01 22:04:59 +0200

    Блок BBcode
    ВРОДЕ КАК эта хуйна должна фиксить ббкод в чате. Проверяла? Нет, код полуспизженный, вроде должен работать

--- Content.Client/Chat/ChatSystem.cs
+++ Content.Client/Chat/ChatSystem.cs
@@ -1,5 +1,19 @@
 using Content.Shared.Chat;
+using System.Text.RegularExpressions;
 
 namespace Content.Client.Chat;
+
+public void OnPlayerChat(EntityUid player, string message)
+{
+    var bbcodePattern = new Regex(@"\[(\/?)(b|i|u|color|size|spoiler)(=[^\]]+)?\]", RegexOptions.IgnoreCase);
+
+    if (bbcodePattern.IsMatch(message))
+    {
+        ShowPlayerMessage(player, "BBCode-теги запрещены в чате.");
+        return;
+    }
+
+    SendToChat(player, message);
+}
 
 public sealed class ChatSystem : SharedChatSystem {}
