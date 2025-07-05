b90965177f458892b6ab3b12cb3265772f698c74
vanx <61917534+Vaaankas@users.noreply.github.com>
2025-07-05 05:22:19 +0300

    [Add] Alert Level Locks (#635)
    * alert lock pog
    
    * locale
    
    * fix
    
    * make it better
    
    * count
    
    * review
    
    * review 2
    
    ---------
    
    Co-authored-by: vanx <discord@vanxxxx>

---
+++ Content.Shared/_White/Lockers/SharedStationAlertLevelLockSystem.cs
@@ -0,0 +1,30 @@
+﻿using Content.Shared.Lock;
+using Content.Shared.Popups;
+
+namespace Content.Shared._White.Lockers;
+
+public sealed class SharedStationAlertLevelLockSystem : EntitySystem
+{
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<StationAlertLevelLockComponent, LockToggleAttemptEvent>(OnTryAccess);
+    }
+
+    private void OnTryAccess(Entity<StationAlertLevelLockComponent> ent, ref LockToggleAttemptEvent args)
+    {
+        if (!TryComp<LockComponent>(ent.Owner, out var lockComponent))
+            return;
+        var locking = !lockComponent.Locked; // Allow locking
+
+        if (!ent.Comp.Enabled || !ent.Comp.Locked || locking)
+            return;
+
+        _popup.PopupClient(Loc.GetString("access-failed-wrong-station-alert-level"), ent.Owner, args.User);
+
+        args.Cancelled = true;
+    }
+}
