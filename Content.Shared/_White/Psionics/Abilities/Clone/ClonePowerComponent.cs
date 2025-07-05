952b0f0b48ade9d7a652630de4931980a816348f
kekoven1 <anilose55@gmail.com>
2025-07-05 11:05:16 +0300

    spatison changed

---
+++ Content.Shared/_White/Psionics/Abilities/Clone/ClonePowerComponent.cs
@@ -0,0 +1,16 @@
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._White.Abilities.Psionics;
+
+[RegisterComponent]
+public sealed partial class ClonePowerComponent : Component {
+    [DataField]
+    public SoundSpecifier CloneSound = new SoundPathSpecifier("/Audio/_White/Object/Devices/experimentalsyndicateteleport.ogg");
+
+    [DataField]
+    public EntProtoId? CloneEffect = "ExperimentalTeleporterInEffect";
+        
+    [ViewVariables]
+    public EntityUid? CloneUid;
+}
