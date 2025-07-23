3b88595aa9e81143d0c47d61df8d59d5cc9b5736
RedBurningPhoenix <147742474+RedBurningPhoenix@users.noreply.github.com>
2025-07-23 05:28:35 +0500

    [Fix] Boxes/magazine's examine. And UseButton for extract. (#673)
    * Fix examine and Z button
    
    * okay?

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Ballistic.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Ballistic.cs
@@ -242,7 +242,7 @@ public abstract partial class SharedGunSystem
             // Show top round
             if (component.Entities.Count > 0)
             {
-                var round = Name(component.Entities[-1]);
+                var round = Name(component.Entities[^1]);
 
                 args.PushMarkup(
                     Loc.GetString("ammo-top-round-examine", ("color", ModeExamineColor), ("round", round)));
@@ -335,7 +335,7 @@ public abstract partial class SharedGunSystem
         UpdateAmmoCount(uid);
     }
 
-    protected abstract void Cycle(EntityUid uid, BallisticAmmoProviderComponent component, MapCoordinates coordinates, GunComponent? gunComponent = null);
+    protected abstract void Cycle(EntityUid uid, BallisticAmmoProviderComponent? component, MapCoordinates coordinates, GunComponent? gunComponent = null);
 
     private void OnBallisticInit(EntityUid uid, BallisticAmmoProviderComponent component, ComponentInit args)
     {
