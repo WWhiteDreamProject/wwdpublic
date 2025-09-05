78916fd4b8f5cca8343a4309d799a833be8881cc
Cinka <leofoxm40@gmail.com>
2025-09-05 18:36:40 +0300

    - fix: буковки

--- Content.Client/_White/Bark/BarkSourceComponent.cs
+++ Content.Client/_White/Bark/BarkSourceComponent.cs
@@ -3,15 +3,18 @@ using Robust.Shared.Audio;
 
 namespace Content.Client._White.Bark;
 
-
 [RegisterComponent]
 public sealed partial class BarkSourceComponent : Component
 {
-    [DataField] public Queue<BarkData> Barks { get; set; } = new();
+    [DataField]
+    public Queue<BarkData> Barks { get; set; } = new();
 
-    [DataField] public SoundSpecifier ResolvedSound { get; set; }
+    [DataField]
+    public SoundSpecifier ResolvedSound { get; set; }
 
-    [ViewVariables] public BarkData? CurrentBark { get; set; }
+    [ViewVariables]
+    public BarkData? CurrentBark { get; set; }
 
-    [ViewVariables] public float BarkTime { get; set; }
+    [ViewVariables]
+    public float BarkTime { get; set; }
 }
