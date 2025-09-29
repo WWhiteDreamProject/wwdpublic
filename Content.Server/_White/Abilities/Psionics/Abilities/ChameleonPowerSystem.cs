01a330688e91a43a5a6c575adc9f41a545a9ead2
kekoven1 <anilose55@gmail.com>
2025-09-29 23:56:36 +0700

    i think its gonna work

---
+++ Content.Server/_White/Abilities/Psionics/Abilities/ChameleonPowerSystem.cs
@@ -0,0 +1,99 @@
+using Content.Server.EntityEffects.Effects;
+using Content.Server.Polymorph.Systems;
+using Content.Shared._White.Actions.Events;
+using Content.Shared._White.Psionics.Abilities;
+using Content.Shared.Abilities.Psionics;
+using Content.Shared.Actions;
+using Content.Shared.Humanoid;
+using Content.Shared.Traits.Assorted.Components;
+using Robust.Server.GameObjects;
+using Robust.Shared.GameObjects;
+using Robust.Shared.GameObjects.Components.Localization;
+using Robust.Shared.Player;
+using Robust.Shared.Timing;
+using System.Numerics;
+
+namespace Content.Server._White.Abilities.Psionics.Abilities
+{
+    public sealed class PolymorphPowerSystem : EntitySystem
+    {
+        [Dependency] private readonly SharedPsionicAbilitiesSystem _psionics = default!;
+        [Dependency] private readonly MetaDataSystem _meta = default!;
+        [Dependency] private readonly SharedHumanoidAppearanceSystem _appearance = default!;
+        [Dependency] private readonly SharedTransformSystem _transform = default!;
+
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            SubscribeLocalEvent<PolymorphPowerActionEvent>(OnPowerUsed);
+            SubscribeLocalEvent<PolymorphPowerComponent, PolymorphPowerRevertActionEvent>(OnUsed);
+            SubscribeLocalEvent<PolymorphPowerComponent, ComponentStartup>(ComponentStart);
+        }
+
+        public void OnPowerUsed(PolymorphPowerActionEvent args)
+        {
+            if (!_psionics.OnAttemptPowerUse(args.Performer, args.Target, "polymorph", true))
+                return;
+            if (!TryComp<HumanoidAppearanceComponent>(args.Target, out var humanoid))
+                return;
+
+            var target = args.Target;
+            var user = args.Performer;
+
+            if (TryComp<HumanoidAppearanceComponent>(target, out var targetHumanoid) &&
+                TryComp<HumanoidAppearanceComponent>(user, out var userHumanoid))
+            {
+
+                var meta = MetaData(target);
+                _meta.SetEntityName(user, meta.EntityName);
+                _meta.SetEntityDescription(user, meta.EntityDescription);
+
+                userHumanoid.Species = targetHumanoid.Species;
+                userHumanoid.Sex = targetHumanoid.Sex;
+                userHumanoid.Age = targetHumanoid.Age;
+                userHumanoid.SkinColor = targetHumanoid.SkinColor;
+                userHumanoid.CustomBaseLayers = targetHumanoid.CustomBaseLayers;
+                userHumanoid.Gender = targetHumanoid.Gender;
+                userHumanoid.Width = targetHumanoid.Width;
+                userHumanoid.Height = targetHumanoid.Height;
+                userHumanoid.MarkingSet = targetHumanoid.MarkingSet;
+                userHumanoid.Voice = targetHumanoid.Voice;
+                userHumanoid.BodyType = targetHumanoid.BodyType;
+
+                if (TryComp<GrammarComponent>(target, out var grammar))
+                {
+                    grammar.Gender = targetHumanoid.Gender;
+                }
+
+                Dirty(user, userHumanoid);
+            }
+
+            _psionics.LogPowerUsed(args.Performer, "polymorph");
+            args.Handled = true;
+        }
+
+        public void OnUsed(EntityUid uid, PolymorphPowerComponent comp, PolymorphPowerRevertActionEvent args)
+        {
+            if (!TryComp<HumanoidAppearanceComponent>(uid, out var humanoid))
+                return;
+
+            if (!TryComp<TransformComponent>(uid, out var transform))
+                return;
+
+            var effect = Spawn("PsionicPolymorphEffect", _transform.GetMapCoordinates(uid));
+            _transform.SetParent(effect, uid);
+
+            _meta.SetEntityName(uid, comp.OriginalName);
+            _meta.SetEntityDescription(uid, comp.OriginalDescription);
+            _appearance.LoadProfile(uid, humanoid.LastProfileLoaded);
+        }
+
+        public void ComponentStart(EntityUid uid, PolymorphPowerComponent component, ComponentStartup args)
+        {
+            var meta = MetaData(uid);
+            component.OriginalName = meta.EntityName;
+            component.OriginalDescription = meta.EntityDescription;
+        }
+    }
+}
---
+++ Content.Shared/_White/Psionics/Abilities/PsionicPolymorphComponent.cs
@@ -0,0 +1,18 @@
+using Content.Shared.Humanoid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content.Shared._White.Psionics.Abilities;
+
+[RegisterComponent]
+public sealed partial class PolymorphPowerComponent : Component
+{
+    [ViewVariables]
+    public string OriginalName = "Jonh Doe";
+
+    [ViewVariables]
+    public string OriginalDescription = "Killer of the death";
+}
