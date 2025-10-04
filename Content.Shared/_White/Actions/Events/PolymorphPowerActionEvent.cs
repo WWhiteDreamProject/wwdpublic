710b81dc81a3e019adf693bb0df8bba77e0c2d52
kekoven1 <anilose55@gmail.com>
2025-10-04 15:26:27 +0700

    psiop (#852)
    * i think its gonna work
    
    * Update PsionicPowerPool.yml
    
    * Update ChameleonPowerSystem.cs
    
    * bunny changes
    
    * another one
    
    * still awful ahh effect
    
    * hewo
    
    * Update meta.json
    
    * ok its rly better
    
    * Update PolymorphPowerSystem.cs
    
    * i think its last one
    
    * oops
    
    * poprav nemnogo i gotovo
    
    * Update Resources/Prototypes/_White/Entities/Effects/psionics.yml
    
    ---------
    
    Co-authored-by: Spatison <137375981+Spatison@users.noreply.github.com>

---
+++ Content.Shared/_White/Actions/Events/PolymorphPowerActionEvent.cs
@@ -0,0 +1,7 @@
+using Content.Shared.Actions;
+
+namespace Content.Shared._White.Actions.Events;
+
+public sealed partial class PolymorphPowerActionEvent : EntityTargetActionEvent;
+
+public sealed partial class PolymorphPowerRevertActionEvent : InstantActionEvent;
