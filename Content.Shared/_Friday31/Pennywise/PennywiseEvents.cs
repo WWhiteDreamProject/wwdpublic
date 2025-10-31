5cb6d3597896a0ff5c17eacad91c97702b30ca23
Kutosss <162154227+Kutosss@users.noreply.github.com>
2025-10-31 19:40:14 +0300

    Revert "Friday31 (#868)"
    This reverts commit 10a2b918fdf3fa4c44da6e422891d2199072408e.

--- Content.Shared/_Friday31/Pennywise/PennywiseEvents.cs
+++
@@ -1,9 +0,0 @@
-using Content.Shared.Actions;
-
-namespace Content.Shared._Friday31.Pennywise;
-
-public sealed partial class PennywiseChameleonEvent : EntityTargetActionEvent;
-
-public sealed partial class PennywisePhaseToggleEvent : InstantActionEvent;
-
-public sealed partial class PennywiseSpawnBalloonEvent : InstantActionEvent;
