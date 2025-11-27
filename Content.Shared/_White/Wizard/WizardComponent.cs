95307f86e17e05586dee02c6db13a41c0a8d43ee
Kai5 <68296202+Kai518@users.noreply.github.com>
2025-11-27 04:40:39 +0300

    Update Content.Shared/_White/Wizard/WizardComponent.cs
    Co-authored-by: Spatison <137375981+Spatison@users.noreply.github.com>

--- Content.Shared/_White/Wizard/WizardComponent.cs
+++ Content.Shared/_White/Wizard/WizardComponent.cs
@@ -2,7 +2,6 @@ using Content.Shared.StatusIcon;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 
-
 namespace Content.Shared._White.Wizard;
 
 [RegisterComponent, NetworkedComponent]
