6e48a052c413e7022a7058d82a21527a122bd7b1
kekoven1 <anilose55@gmail.com>
2025-12-24 23:14:39 +0700

    Update ClonePowerSystem.cs

--- Content.Server/_White/Abilities/Psionics/Abilities/ClonePowerSystem.cs
+++ Content.Server/_White/Abilities/Psionics/Abilities/ClonePowerSystem.cs
@@ -186,10 +186,9 @@ public sealed class ClonePowerSystem : EntitySystem
         if (TryComp<ClonePowerComponent>(component.OriginalUid, out var origComp))
             origComp.CloneUid = null;
 
-        if (_mind.TryGetMind(uid, out var mindId, out var mind))
+        if (_mind.TryGetMind(uid, out var mindId, out var mind) & !_mind.TryGetMind(component.OriginalUid.Value, out _, out _))
         {
-            if (component.OriginalUid != null)
-                _mind.TransferTo(mindId, component.OriginalUid.Value, mind: mind);
+            _mind.TransferTo(mindId, component.OriginalUid.Value, mind: mind);
         }
 
         StripCloneEquipment(uid);
