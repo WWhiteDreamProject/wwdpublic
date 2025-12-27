042cec90a39d63552be4db9f145cba11ba60aae6
Spatison <137375981+Spatison@users.noreply.github.com>
2025-12-27 05:45:34 +0300

    Исправление голосовой маски (#916)
    fix

--- Content.Shared/Chat/SharedChatEvents.cs
+++ Content.Shared/Chat/SharedChatEvents.cs
@@ -1,6 +1,7 @@
 using Content.Shared.Speech;
 using Robust.Shared.Prototypes;
 using Content.Shared.Inventory;
+using Content.Shared.Radio;
 
 namespace Content.Shared.Chat;
 
@@ -8,31 +9,20 @@ namespace Content.Shared.Chat;
 ///     This event should be sent everytime an entity talks (Radio, local chat, etc...).
 ///     The event is sent to both the entity itself, and all clothing (For stuff like voice masks).
 /// </summary>
-public class TransformSpeakerNameEvent : EntityEventArgs, IInventoryRelayEvent // WWDP add sealed
+public sealed class TransformSpeakerNameEvent : EntityEventArgs, IInventoryRelayEvent
 {
     public SlotFlags TargetSlots { get; } = SlotFlags.WITHOUT_POCKET;
     public EntityUid Sender;
     public string VoiceName;
     public ProtoId<SpeechVerbPrototype>? SpeechVerb;
+    public readonly RadioChannelPrototype? Channel; // WD EDIT
 
-    public TransformSpeakerNameEvent(EntityUid sender, string name)
+    public TransformSpeakerNameEvent(EntityUid sender, string name, RadioChannelPrototype? channel = null) // WD EDIT
     {
         Sender = sender;
         VoiceName = name;
         SpeechVerb = null;
-    }
-}
-
-/// <summary>
-/// WWDP add
-/// </summary>
-[ByRefEvent]
-public sealed class TransformRadioSpeakerNameEvent : TransformSpeakerNameEvent
-{
-    public readonly Shared.Radio.RadioChannelPrototype Channel;
-
-    public TransformRadioSpeakerNameEvent(EntityUid sender, string name, Shared.Radio.RadioChannelPrototype channel) : base(sender, name)
-    {
         Channel = channel;
     }
 }
+
