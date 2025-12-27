685517f4d6b6b8dca9dd1a0769f5ede32c6ffd50
temm1ie <63717386+temm1ie@users.noreply.github.com>
2025-12-27 17:35:57 +0300

    end

--- Content.Server/_War/DiskTag/DiskTagComponent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Content.Server._War.DiskTag;
-
-[RegisterComponent]
-public sealed partial class DiskTagComponent : Component
-{
-    // Этот компонент не требует дополнительных полей, так как он просто идентифицирует диск
-}
-
