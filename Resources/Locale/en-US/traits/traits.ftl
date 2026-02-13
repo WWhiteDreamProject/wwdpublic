LaCumbiaDelCoronavirus/KS16

Added Pain Numbness Trait (#34538)
* added pain-numbness component and system

* added numb as a trait that pulls the pain numbness component

* removed new event as mob threshold event as already being fired

* checked for MobThresholdsComponent first before running VerifyThresholds

* refacted force say to using LocalizedDatasetPrototype and added numb messages

* added severity check alert

* added comment for BeforeForceSayEvent

* removed space formatting

* changed Cancelled to CancelUpdate, fixed spacing and added two more damage-force-say-numb

* changed prefix damage-force-say-numb to 5 (whoops)

Resources/Locale/en-US/traits/traits.ftl
@@ -59,3 +59,6 @@ trait-french-desc = Your accent seems to have a certain «je ne sais quoi».
 
 trait-spanish-name = Spanish accent
 trait-spanish-desc = Hola señor, donde esta la biblioteca.
+
+trait-painnumbness-name = Numb
+trait-painnumbness-desc = You lack any sense of feeling pain, being unaware of how hurt you may be.

WWhiteDreamProject/wwdpublic

(cherry picked from commit 012c8355594823bc327fc42693a43a7c00f0caf9)

Resources/Locale/en-US/traits/traits.ftl
@@ -100,6 +100,9 @@ trait-description-ItalianAccent = You speak with a Italian accent.
 trait-name-SpanishAccent = Spanish accent
 trait-description-SpanishAccent = You speak with a Spanish accent.
 
+trait-name-PainNumbness = Numb
+trait-description-PainNumbness = You lack any sense of feeling pain, being unaware of how hurt you may be.
+
 trait-name-Snoring = Snoring
 trait-description-Snoring = You tend to snore loudly while sleeping.
 