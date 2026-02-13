95bee5bda3e85192f11abb1e78ea092c408a1cf7
Spatison <137375981+Spatison@users.noreply.github.com>
2026-02-13 23:13:05 +0300

    fix wizmerge

--- Content.Shared/Projectiles/SharedProjectileSystem.cs
+++ Content.Shared/Projectiles/SharedProjectileSystem.cs
@@ -140,7 +140,7 @@ public abstract partial class SharedProjectileSystem : EntitySystem
         // Raise a specific event for projectiles.
         if (TryComp(embeddable, out ProjectileComponent? projectile))
         {
-            var ev = new ProjectileEmbedEvent(projectile.Shooter!.Value, projectile.Weapon!.Value, args.Target);
+            var ev = new ProjectileEmbedEvent(projectile.Shooter, projectile.Weapon, args.Target); // WD EDIT
             RaiseLocalEvent(embeddable, ref ev);
         }
     }
