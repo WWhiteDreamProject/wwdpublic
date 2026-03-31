b5438bb3bb15650ecafe5d21be4e5a29852c1bc4
DDDecard <gavrilovgavrilov2005@gmail.com>
2026-03-31 08:58:59 +0300

    add link
    Co-authored-by: RedFoxIV <38788538+RedFoxIV@users.noreply.github.com>

--- Content.Server/Ame/AmeNodeGroup.cs
+++ Content.Server/Ame/AmeNodeGroup.cs
@@ -168,9 +168,9 @@ public sealed class AmeNodeGroup : BaseNodeGroup
     /// </summary>
     public float CalculatePower(int fuel, int cores)
     {
-        // Balanced around a single core AME with 2 injections producing 120KW.
-        // With each increase in the number of cores and injections, the increase in power will be less and less
-        
+        // Balanced around a single core AME with injection level 2 producing 120KW.
+        // Overclocking yields diminishing returns.
+        // Look at this      graph: https://www.desmos.com/calculator/bw9azwyt3k
         // The adjustment for cores make it so that a 1 core AME at 2 injections is better than a 2 core AME at 2 injections.
         // However, for the relative amounts for each (1 core at 2 and 2 core at 4), more cores has more output.
         return MathF.Max(200000f * MathF.Log10(2 * fuel * MathF.Pow(cores, (float)-0.5)), 0);
