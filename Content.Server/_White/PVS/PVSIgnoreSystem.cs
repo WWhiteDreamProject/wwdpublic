f24362f0a51f60ee89fdaf74408c6e8e00bac1e0
Gersoon <144093296+Gersoon458@users.noreply.github.com>
2026-04-04 15:50:59 +0200

    Apply suggestions from code review
    Co-authored-by: RedFoxIV <38788538+RedFoxIV@users.noreply.github.com>

--- Content.Server/_White/PVS/PVSIgnoreSystem.cs
+++ Content.Server/_White/PVS/PVSIgnoreSystem.cs
@@ -1,6 +1,6 @@
 using Robust.Server.GameStates;
 
-namespace Content.Server.GameStates;
+namespace Content.Server._White.PVS;
 
 public sealed class PVSIgnoreSystem : EntitySystem
 {
@@ -14,13 +14,7 @@ public sealed class PVSIgnoreSystem : EntitySystem
         SubscribeLocalEvent<PVSIgnoreComponent, ComponentShutdown>(OnShutdown);
     }
 
-    private void OnStartup(EntityUid uid, PVSIgnoreComponent comp, ComponentStartup args)
-    {
-        _pvsOverride.AddGlobalOverride(uid);
-    }
+    private void OnStartup(EntityUid uid, PVSIgnoreComponent comp, ComponentStartup args) => _pvsOverride.AddGlobalOverride(uid);
 
-    private void OnShutdown(EntityUid uid, PVSIgnoreComponent comp, ComponentShutdown args)
-    {
-        _pvsOverride.RemoveGlobalOverride(uid);
-    }
+    private void OnShutdown(EntityUid uid, PVSIgnoreComponent comp, ComponentShutdown args) => _pvsOverride.RemoveGlobalOverride(uid);
 }
