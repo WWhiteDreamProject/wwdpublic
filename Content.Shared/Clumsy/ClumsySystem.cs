96cd10563d424491a03dc28ca770d37a2b23a229
Spatison <137375981+Spatison@users.noreply.github.com>
2026-04-11 17:44:00 +0300

    Update Content.Shared/Clumsy/ClumsySystem.cs

--- Content.Shared/Clumsy/ClumsySystem.cs
+++ Content.Shared/Clumsy/ClumsySystem.cs
@@ -23,8 +23,8 @@ public sealed class ClumsySystem : EntitySystem
     [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
     [Dependency] private readonly DamageableSystem _damageable = default!;
-[Dependency] private readonly IGameTiming _timing = default!;
-[Dependency] private readonly IConfigurationManager _cfg = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
     [Dependency] private readonly MobStateSystem _mobState = default!; // WD EDIT
 
     public override void Initialize()
