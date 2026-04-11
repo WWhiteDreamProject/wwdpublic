e7a758d21c6a0bf98d98d1a70528f7319c21707c
Gersoon <144093296+Gersoon458@users.noreply.github.com>
2026-04-11 18:16:17 +0200

    cheto

--- Content.Shared/Implants/Components/RattleComponent.cs
+++ Content.Shared/Implants/Components/RattleComponent.cs
@@ -7,11 +7,13 @@ using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototy
 public sealed partial class RattleComponent : Component
 {
     // The radio channels the message will be sent to
-    [DataField(customTypeSerializer: typeof(PrototypeIdListSerializer<RadioChannelPrototype>))] //WWDP edit
-    public List<string> RadioChannel = new() { "Syndicate" }; //WWDP edit
+    [DataField]
+    public List<ProtoId<RadioChannelPrototype>> RadioChannel = new() { "Syndicate" }; //WWDP edit
 
+    // WWDP edit start
     [DataField]
     public LocId ReviveMessage = "deathrattle-implant-revive-message";
+    // WWDP edit end
 
     // The message that the implant will send when crit
     [DataField]
@@ -20,9 +22,4 @@ public sealed partial class RattleComponent : Component
     // The message that the implant will send when dead
     [DataField("deathMessage")]
     public LocId DeathMessage = "deathrattle-implant-dead-message";
-
-    # WD EDIT START
-    [DataField]
-    public LocId ReviveMessage = "deathrattle-implant-revive-message";
-    # WD EDIT END
 }
